03cc7046bba68e1fdd67ec92bd9f8109df594031
JuneBridge <junebridge03@g.skku.edu>
2024-07-06 15:43:17 +0900

    init: initial commit

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/FourtoonCookieApplication.java
@@ -0,0 +1,13 @@
+package com.startingblue.fourtooncookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourtoonCookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourtoonCookieApplication.class, args);
+	}
+
+}
