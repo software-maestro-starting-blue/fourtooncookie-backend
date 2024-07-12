d3afba6a4e0bb24238d99c76e0ed25b68e4219f0
minseo Jo <liging12@naver.com>
2024-07-12 20:00:58 +0900

    feat: 일기 페이지 요청 DTO 추가

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiaryPageRequest.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiaryPageRequest.java
@@ -1,30 +1,32 @@
 package com.startingblue.fourtooncookie.diary.dto.request;
 
-public record DiaryPageRequest(Integer page, Integer size) {
+public record DiaryPageRequest(Integer pageNumber, Integer pageSize) {
 
     private static final int MIN_PAGE_NUMBER = 0;
+    private static final int MAX_PAGE_NUMBER = 200;
+
     private static final int MIN_PAGE_SIZE = 1;
-    private static final int MAX_PAGE_SIZE = 100;
+    private static final int MAX_PAGE_SIZE = 10;
 
-    private static final int DEFAULT_PAGE = 0;
-    private static final int DEFAULT_SIZE = 10;
+    private static final int DEFAULT_PAGE_NUMBER = 0;
+    private static final int DEFAULT_PAGE_SIZE = 10;
 
-    public DiaryPageRequest(Integer page, Integer size) {
-        this.page = defaultIfNullOrInvalid(page, DEFAULT_PAGE, MIN_PAGE_NUMBER, Integer.MAX_VALUE);
-        this.size = defaultIfNullOrInvalid(size, DEFAULT_SIZE, MIN_PAGE_SIZE, MAX_PAGE_SIZE);
+    public DiaryPageRequest(Integer pageNumber, Integer pageSize) {
+        this.pageNumber = defaultIfNullOrInvalid(pageNumber, DEFAULT_PAGE_NUMBER, MIN_PAGE_NUMBER, MAX_PAGE_NUMBER);
+        this.pageSize = defaultIfNullOrInvalid(pageSize, DEFAULT_PAGE_SIZE, MIN_PAGE_SIZE, MAX_PAGE_SIZE);
         validate();
     }
 
     private void validate() {
-        if (page < MIN_PAGE_NUMBER) {
-            throw new IllegalArgumentException("Page number cannot be less than " + MIN_PAGE_NUMBER);
+        if (pageNumber < MIN_PAGE_NUMBER || pageNumber > MAX_PAGE_NUMBER) {
+            throw new IllegalArgumentException("pageNumber number cannot be less than " + MIN_PAGE_NUMBER + " and " + MAX_PAGE_NUMBER);
         }
-        if (size < MIN_PAGE_SIZE || size > MAX_PAGE_SIZE) {
-            throw new IllegalArgumentException("Size must be between " + MIN_PAGE_SIZE + " and " + MAX_PAGE_SIZE);
+        if (pageSize < MIN_PAGE_SIZE || pageSize > MAX_PAGE_SIZE) {
+            throw new IllegalArgumentException("pageSize must be between " + MIN_PAGE_SIZE + " and " + MAX_PAGE_SIZE);
         }
     }
 
-    private static Integer defaultIfNullOrInvalid(Integer value, int defaultValue, int minValue, int maxValue) {
+    private static Integer defaultIfNullOrInvalid(Integer value, Integer defaultValue, Integer minValue, Integer maxValue) {
         if (value == null || value < minValue || value > maxValue) {
             return defaultValue;
         }
