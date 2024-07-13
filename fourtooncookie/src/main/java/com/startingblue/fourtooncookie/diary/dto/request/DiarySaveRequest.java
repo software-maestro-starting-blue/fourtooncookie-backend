a7724de732e7a67ccafa428507f10971524a1405
minseo Jo <liging12@naver.com>
2024-07-13 16:54:10 +0900

    chore: 매개변수 위치 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiarySaveRequest.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiarySaveRequest.java
@@ -3,5 +3,5 @@ package com.startingblue.fourtooncookie.diary.dto.request;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record DiarySaveRequest(String content, List<Long> hashtagIds, LocalDateTime diaryDate) {
+public record DiarySaveRequest(String content, LocalDateTime diaryDate, List<Long> hashtagIds) {
 }
