c5d2b6a546bdea835ceb01a55fc004e02137f470
minseo Jo <liging12@naver.com>
2024-07-14 18:11:39 +0900

    feat: favorite 기능 추가

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/DiaryController.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/DiaryController.java
@@ -5,6 +5,8 @@ import com.startingblue.fourtooncookie.diary.dto.request.DiarySaveRequest;
 import com.startingblue.fourtooncookie.diary.dto.request.DiaryUpdateRequest;
 import com.startingblue.fourtooncookie.diary.dto.response.DiarySavedResponse;
 import com.startingblue.fourtooncookie.diary.service.DiaryService;
+import com.startingblue.fourtooncookie.member.domain.Member;
+import com.startingblue.fourtooncookie.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,24 +21,31 @@ import java.util.List;
 public class DiaryController {
 
     private final DiaryService diaryService;
+    private final MemberService memberService;
 
     @PostMapping
     public ResponseEntity<Void> createDiary(@RequestBody final DiarySaveRequest request) {
-        diaryService.createDiary(request, 0L); // TODO : 우선 디폴트 값 넣어 놓음.
+        memberService.save(new Member());
+        diaryService.createDiary(request, 1L); // TODO : 우선 디폴트 값 넣어 놓음.
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/timeline")
+    public ResponseEntity<List<DiarySavedResponse>> readAllDiaries() {
+        return ResponseEntity.ok(diaryService.readAllDiaries());
+    }
+
     @GetMapping("/timeline/{memberId}")
-    public ResponseEntity<List<DiarySavedResponse>> readDiaries(
+    public ResponseEntity<List<DiarySavedResponse>> readDiariesByMember (
             @PathVariable final Long memberId,
             @RequestParam(defaultValue = "0") final Integer pageNumber,
             @RequestParam(defaultValue = "10") final Integer pageSize) {
         DiaryPageRequest diaryPageRequest = new DiaryPageRequest(pageNumber, pageSize);
-        List<DiarySavedResponse> responses = diaryService.readDiaries(diaryPageRequest, memberId);
+        List<DiarySavedResponse> responses = diaryService.readDiariesByMember(diaryPageRequest, memberId);
         return ResponseEntity.ok(responses);
     }
 
-    @PutMapping("/{diaryId}")
+    @PatchMapping("/{diaryId}")
     public ResponseEntity<Void> updateDiary(@PathVariable final Long diaryId, @RequestBody final DiaryUpdateRequest request) {
         diaryService.updateDiary(diaryId, request);
         return ResponseEntity.ok().build();
@@ -46,5 +55,11 @@ public class DiaryController {
     public ResponseEntity<Void> deleteDiary(@PathVariable final Long diaryId) {
         diaryService.deleteDiary(diaryId);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/favorite/{diaryId}")
+    public ResponseEntity<Void> favoriteDiary(@PathVariable final Long diaryId, @RequestBody final boolean isFavorite) {
+        log.info("favorite: {} diary {}", isFavorite, diaryId);
+        return ResponseEntity.ok().build();
     }
 }
