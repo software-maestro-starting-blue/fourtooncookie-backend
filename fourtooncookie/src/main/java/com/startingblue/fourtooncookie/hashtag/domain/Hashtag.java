b534ebfc3a8666b3f074e9a4732eff5e150fce56
minseo Jo <liging12@naver.com>
2024-07-15 20:15:17 +0900

    fix: 기본 생성자 protected 막기

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/hashtag/domain/Hashtag.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/hashtag/domain/Hashtag.java
@@ -1,12 +1,13 @@
 package com.startingblue.fourtooncookie.hashtag.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Hashtag {
 
     @Id
