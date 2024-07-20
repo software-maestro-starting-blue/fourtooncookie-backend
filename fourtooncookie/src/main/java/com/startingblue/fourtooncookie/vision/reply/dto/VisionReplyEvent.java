f7f8d5bf51503efe847827e50e36967d44d0ba87
JuneBridge <junebridge03@g.skku.edu>
2024-07-20 19:20:02 +0900

    refactor: 필요없는 어노테이션 삭제

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/reply/dto/VisionReplyEvent.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/reply/dto/VisionReplyEvent.java
@@ -1,9 +1,6 @@
 package com.startingblue.fourtooncookie.vision.reply.dto;
 
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
 public record VisionReplyEvent(Long diaryId, List<byte[]> images) {
 }
