be44afdcd29c93503e57670d94963cd77126715c
minseo Jo <liging12@naver.com>
2024-07-20 16:33:40 +0900

    feat: Member Service 코드 추가 및 S3 의존성 추가 (#6)
    * feat: memberService 코드 추가
    
    * feat: S3 관련 build.gradle 추가
    
    * chore: 필요 없는 코드 제거

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/service/MemberService.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.startingblue.fourtooncookie.member.service;
 
+import com.startingblue.fourtooncookie.member.domain.Member;
 import com.startingblue.fourtooncookie.member.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,13 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new IllegalArgumentException("member not found"));
+    }
+
+    public void save(Member member) {
+        memberRepository.save(member);
+    }
 
 }
