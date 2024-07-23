65035084db3174b6b4d2c6e3f536401c94f85248
minseo Jo <liging12@naver.com>
2024-07-23 13:58:33 +0900

    chore: 필요 없는 import 문 제거

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/domain/CharacterRepository.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/domain/CharacterRepository.java
@@ -3,8 +3,6 @@ package com.startingblue.fourtooncookie.character.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Long> {
 }
