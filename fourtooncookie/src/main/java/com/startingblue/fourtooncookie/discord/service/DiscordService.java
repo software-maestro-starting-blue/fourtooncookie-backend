9d4bf7aa619c4cc4b7536d05d1e4223b5b9ce1a8
JuneBridge <junebridge03@g.skku.edu>
2024-07-23 16:28:40 +0900

    [feat] Discord 서비스 추가 (#11)
    * init: Discord API 통신 로직 구현
    
    * feat: 빈으로 등록된 ListenerAdapter가 바로 등록되도록 로직을 추가하였습니다.

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/discord/service/DiscordService.java
@@ -0,0 +1,67 @@
+package com.startingblue.fourtooncookie.discord.service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class DiscordService {
+
+    private JDA jda;
+
+    @Value("${discord.token}")
+    private String DISCORD_API_KEY;
+
+    private final List<ListenerAdapter> listeners;
+
+    @PostConstruct
+    private void init() throws InterruptedException {
+        jda = JDABuilder.createDefault(DISCORD_API_KEY)
+                .enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.DIRECT_MESSAGES)
+                .build();
+        jda.awaitReady();
+        listeners.forEach(this::addListener);
+    }
+
+    public void sendMessage(Integer guildId, Integer channelId, String message) {
+        Guild guild = jda.getGuildById(guildId);
+        if (guild == null) {
+            throw new IllegalStateException("Guild not found");
+        }
+
+        TextChannel textChannel = guild.getTextChannelById(channelId);
+
+        if (textChannel == null) {
+            throw new IllegalStateException("Channel not found");
+        }
+
+        textChannel.sendMessage(message).queue();
+    }
+
+    public void addListener(ListenerAdapter listener) {
+        jda.addEventListener(listener);
+    }
+
+    public TextChannel getChannelByName(Integer guildId, String channelName) {
+        Guild guild = jda.getGuildById(guildId);
+        if (guild == null) {
+            throw new IllegalStateException("Guild not found");
+        }
+
+        return guild.getTextChannels().stream()
+                .filter(channel -> channel.getName().equals(channelName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Channel not found"));
+    }
+
+}
