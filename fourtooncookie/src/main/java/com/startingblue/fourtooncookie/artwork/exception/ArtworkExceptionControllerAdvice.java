ca67ebe1101393ed0832ebe675edaf5c93d90ce7
minseo Jo <liging12@naver.com>
2024-07-24 17:55:09 +0900

    feat: Artwork 예외 (notFound) 구현

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/exception/ArtworkExceptionControllerAdvice.java
@@ -0,0 +1,16 @@
+package com.startingblue.fourtooncookie.artwork.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("com.startingblue.fourtooncookie.artwork")
+public class ArtworkExceptionControllerAdvice {
+
+    @ExceptionHandler(ArtworkNoSuchElementException.class)
+    public ResponseEntity<?> handleArtworkNoSuchElementException(ArtworkNoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+}
