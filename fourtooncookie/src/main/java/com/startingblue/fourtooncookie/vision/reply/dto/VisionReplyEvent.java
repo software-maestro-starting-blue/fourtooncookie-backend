3985a0f21bb869865a8713fa042a01ec53061474
JuneBridge <junebridge03@g.skku.edu>
2024-07-24 22:31:20 +0900

    fix: gridPosition을 추가로 받아서 각 이미지에 대한 로직을 처리하도록 수정함

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/reply/dto/VisionReplyEvent.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/reply/dto/VisionReplyEvent.java
@@ -1,6 +1,4 @@
 package com.startingblue.fourtooncookie.vision.reply.dto;
 
-import java.util.List;
-
-public record VisionReplyEvent(Long diaryId, List<byte[]> images) {
+public record VisionReplyEvent(Long diaryId, byte[] image, Integer gridPosition) {
 }
--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/reply/service/VisionReplyService.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/reply/service/VisionReplyService.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Service
 public class VisionReplyService {
 
-    public void processVisionReply(Long diaryId, List<byte[]> images) {
+    public void processVisionReply(Long diaryId, byte[] image, Integer gridPosition) {
         // TODO: S3에 이미지 저장하는 로직 구현
         // TODO: 이미지 URL을 diary에 저장하는 로직 구현
     }
