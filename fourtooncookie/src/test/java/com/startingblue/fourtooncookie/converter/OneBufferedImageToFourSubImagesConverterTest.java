c228cf0190aaa59664018cfab04d82ab9cbc8383
JuneBridge <junebridge03@g.skku.edu>
2024-07-25 14:27:23 +0900

    feat: 4장의 이미지가 하나의 이미지로 합쳐지는 가를 테스트하는 로직을 구현

--- fourtooncookie/src/test/java/com/startingblue/fourtooncookie/converter/OneBufferedImageToFourSubImagesConverterTest.java
+++ fourtooncookie/src/test/java/com/startingblue/fourtooncookie/converter/OneBufferedImageToFourSubImagesConverterTest.java
@@ -55,7 +55,38 @@ class OneBufferedImageToFourSubImagesConverterTest {
 
     }
 
-    void testConvertToEntityAttribute() {
+    @Test
+    @DisplayName("4장의 이미지가 하나로 잘 합쳐지는 가를 확인")
+    void testConvertToEntityAttribute() throws Exception {
+        System.setProperty("java.awt.headless", "false");
+        String imageUrl = "https://cdn.pixabay.com/photo/2016/07/11/17/45/abstract-1510190_1280.png";
+
+        BufferedImage image = ImageIO.read(new URL(imageUrl));
+
+        List<BufferedImage> images = List.of(image, image, image, image);
+
+        BufferedImage combinedImage = oneBufferedImageToFourSubImagesConverter.convertToEntityAttribute(images);
+
+
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(new Dimension(combinedImage.getWidth(), combinedImage.getHeight()));
+
+            // ImageIcon을 사용하여 이미지를 JLabel에 설정
+            ImageIcon icon = new ImageIcon(combinedImage);
+            JLabel label = new JLabel(icon);
+
+            // JLabel을 JFrame에 추가
+            frame.add(label);
+
+            // JFrame을 화면에 표시
+            frame.pack();
+            frame.setLocationRelativeTo(null); // 화면 중앙에 표시
+            frame.setVisible(true);
+        });
+
+        sleep(60 * 1000);
     }
 
 
