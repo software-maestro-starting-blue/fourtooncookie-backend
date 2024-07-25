cf9d21782196c43a61b4a82f2fc8a5df131c2b15
JuneBridge <junebridge03@g.skku.edu>
2024-07-25 13:58:08 +0900

    feat: 하나의 이미지를 4등분된 4개의 이미지들로 convert 진행하는 로직 구현

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/OneBufferedImageToFourSubImagesConverter.java
@@ -0,0 +1,56 @@
+package com.startingblue.fourtooncookie.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+@Converter
+@Service
+public class OneBufferedImageToFourSubImagesConverter implements AttributeConverter<BufferedImage, List<BufferedImage>> {
+
+    @Override
+    public List<BufferedImage> convertToDatabaseColumn(BufferedImage attribute) {
+        int width = attribute.getWidth();
+        int height = attribute.getHeight();
+        List<BufferedImage> subImages = new ArrayList<>();
+
+        subImages.add(attribute.getSubimage(0, 0, width / 2, height / 2)); // top-left
+        subImages.add(attribute.getSubimage(width / 2, 0, width / 2, height / 2)); // top-right
+        subImages.add(attribute.getSubimage(0, height / 2, width / 2, height / 2)); // bottom-left
+        subImages.add(attribute.getSubimage(width / 2, height / 2, width / 2, height / 2));
+
+        return subImages;
+    }
+
+    @Override
+    public BufferedImage convertToEntityAttribute(List<BufferedImage> dbData) {
+        int subImageWidth = dbData.get(0).getWidth();
+        int subImageHeight = dbData.get(0).getHeight();
+        int originalWidth = subImageWidth * 2;
+        int originalHeight = subImageHeight * 2;
+
+        BufferedImage reconstructedImage = new BufferedImage(originalWidth, originalHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = reconstructedImage.createGraphics();
+
+        // Top-left
+        g2d.drawImage(dbData.get(0), 0, 0, null);
+
+        // Top-right
+        g2d.drawImage(dbData.get(1), subImageWidth, 0, null);
+
+        // Bottom-left
+        g2d.drawImage(dbData.get(2), 0, subImageHeight, null);
+
+        // Bottom-right
+        g2d.drawImage(dbData.get(3), subImageWidth, subImageHeight, null);
+
+        g2d.dispose();
+
+        return reconstructedImage;
+    }
+}
