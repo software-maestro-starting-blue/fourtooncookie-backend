ffae74b7587f6c0e066cad804e0bf373392f0076
JuneBridge <junebridge03@g.skku.edu>
2024-07-25 16:55:57 +0900

    feat: S3Config 생성

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/S3Config.java
@@ -0,0 +1,20 @@
+package com.startingblue.fourtooncookie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+    }
+
+}
