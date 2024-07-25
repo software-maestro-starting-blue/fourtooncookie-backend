eb4ef908b3a534e55e1720de370daf9694217372
minseo Jo <liging12@naver.com>
2024-07-25 17:32:01 +0900

    feat: CharacterExceptionControllerAdvice 추가

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/exception/CharacterExceptionControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.startingblue.fourtooncookie.character.exception;
+
+import com.startingblue.fourtooncookie.artwork.exception.ArtworkNoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("com.startingblue.fourtooncookie.character")
+public class CharacterExceptionControllerAdvice {
+
+    @ExceptionHandler(CharacterNoSuchElementException.class)
+    public ResponseEntity<String> handleCharacterNoSuchElementException(CharacterNoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CharacterVisionTypeNoSuchElementException.class)
+    public ResponseEntity<String> handleCharacterVisionTypeNoSuchElementExceptionNoSuchElementException(CharacterVisionTypeNoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
