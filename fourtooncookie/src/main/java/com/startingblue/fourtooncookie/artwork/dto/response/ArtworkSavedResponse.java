33e582f71344cb4c3eb3808fb50a27551ffc4337
minseo Jo <liging12@naver.com>
2024-07-25 13:21:16 +0900

    [feat] 캐릭터 작품 ArtWork 클래스 패키지 구성 및 구현 (#18)
    * feat: Artwork 도메인 생성
    
    * feat: Artwork 컨트롤러 생성
    
    * feat: Artwork dto (request, response) 구현
    
    * feat: Artwork service 구현
    
    * feat: Artwork 예외 (notFound) 구현
    
    * feat: ArtworkRepository 구현
    
    * test: ArtworkRepository 테스트 코드 구현
    
    * test: ArtworkService 테스트 코드 구현
    
    * test: 코드 리뷰 반영
    
    * refactor: 코드 리뷰 반영 반환 값 ? 변경
    
    * test: 코드 리뷰 ArtworkRepositoryTest 반영
    
    * test: 코드 리뷰 ArtworkRepositoryTest given 상수화 반영

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/dto/response/ArtworkSavedResponse.java
@@ -0,0 +1,12 @@
+package com.startingblue.fourtooncookie.artwork.dto.response;
+
+import com.startingblue.fourtooncookie.artwork.domain.Artwork;
+
+import java.net.URL;
+
+public record ArtworkSavedResponse(String title, URL thumnailUrl) {
+
+    public static ArtworkSavedResponse of(Artwork artwork) {
+        return new ArtworkSavedResponse(artwork.getTitle(), artwork.getThumbnailUrl());
+    }
+}
---
+++ fourtooncookie/src/test/java/com/startingblue/fourtooncookie/artwork/domain/ArtworkRepositoryTest.java
@@ -0,0 +1,131 @@
+package com.startingblue.fourtooncookie.artwork.domain;
+
+import com.startingblue.fourtooncookie.artwork.dto.request.ArtworkSaveRequest;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+class ArtworkRepositoryTest {
+
+    @Autowired
+    private ArtworkRepository artworkRepository;
+
+    @DisplayName("새로운 작품을 저장한다.")
+    @Test
+    public void saveArtwork() throws MalformedURLException {
+        // Given
+        String newTitle = "New Artwork";
+        URL newUrl = new URL("http://test.com/newimage.jpg");
+        ArtworkSaveRequest request = new ArtworkSaveRequest(newTitle, newUrl);
+
+        Artwork artwork = new Artwork(request.title(), request.thumnailUrl());
+
+        // When
+        Artwork savedArtwork = artworkRepository.save(artwork);
+
+        // Then
+        Optional<Artwork> foundArtwork = artworkRepository.findById(savedArtwork.getId());
+
+        assertThat(foundArtwork).isPresent();
+        assertThat(foundArtwork.get().getTitle()).isEqualTo(newTitle);
+        assertThat(foundArtwork.get().getThumbnailUrl()).isEqualTo(newUrl);
+    }
+
+    @DisplayName("저장된 작품을 Id로 찾는다.")
+    @Test
+    public void findById() throws MalformedURLException {
+        // Given
+        String title = "New Artwork";
+        URL url = new URL("http://test.com/newimage.jpg");
+        Artwork artwork = new Artwork(title, url);
+        Artwork savedArtwork = artworkRepository.save(artwork);
+
+        // When
+        Optional<Artwork> foundArtwork = artworkRepository.findById(savedArtwork.getId());
+
+        // Then
+        assertThat(foundArtwork).isPresent();
+        assertThat(foundArtwork.get().getTitle()).isEqualTo(title);
+        assertThat(foundArtwork.get().getThumbnailUrl()).isEqualTo(url);
+    }
+
+    @DisplayName("저장된 모든 작품을 가져온다.")
+    @Test
+    public void findALl() throws MalformedURLException {
+        // Given
+        String title1 = "Title 1";
+        URL url1 = new URL("http://test.com/image1.jpg");
+        String title2 = "Title 2";
+        URL url2 = new URL("http://test.com/image2.jpg");
+        Artwork artwork1 = new Artwork(title1, url1);
+        Artwork artwork2 = new Artwork(title2, url2);
+        artworkRepository.saveAll(List.of(artwork1, artwork2));
+
+        // When
+        List<Artwork> savedArtworks = artworkRepository.findAll();
+
+
+        // Then
+        assertThat(savedArtworks).hasSize(2);
+        assertThat(savedArtworks.get(0).getTitle()).isEqualTo(title1);
+        assertThat(savedArtworks.get(0).getThumbnailUrl()).isEqualTo(url1);
+        assertThat(savedArtworks.get(1).getTitle()).isEqualTo(title2);
+        assertThat(savedArtworks.get(1).getThumbnailUrl()).isEqualTo(url2);
+    }
+
+    @DisplayName("저장된 작품을 업데이트한다.")
+    @Test
+    public void updateArtwork() throws MalformedURLException {
+        // Given
+        String oldTitle = "Old Title";
+        URL oldUrl = new URL("http://test.com/oldimage.jpg");
+        Artwork artwork = new Artwork(oldTitle, oldUrl);
+        Artwork savedArtwork = artworkRepository.save(artwork);
+
+        String updateTitle = "update Title";
+        URL updateUrl = new URL("http://test.com/updateimage.jpg");
+        savedArtwork.update(updateTitle, updateUrl);
+
+        // When
+        artworkRepository.save(savedArtwork);
+
+        // Then
+        Optional<Artwork> updatedArtwork = artworkRepository.findById(savedArtwork.getId());
+        assertThat(updatedArtwork).isPresent();
+        assertThat(updatedArtwork.get().getTitle()).isEqualTo(updateTitle);
+        assertThat(updatedArtwork.get().getThumbnailUrl()).isEqualTo(updateUrl);
+    }
+
+
+    @DisplayName("저장된 작품을 삭제한다.")
+    @Test
+    public void deleteById() throws MalformedURLException {
+        // Given
+        String title = "Title";
+        URL url = new URL("http://test.com/newimage.jpg");
+        Artwork artwork = new Artwork(title, url);
+        Artwork savedArtwork = artworkRepository.save(artwork);
+
+        Long deletedId = savedArtwork.getId();
+
+        // When
+        artworkRepository.delete(savedArtwork);
+        Optional<Artwork> foundArtwork = artworkRepository.findById(deletedId);
+
+        // Then
+        assertThat(foundArtwork).isNotPresent();
+    }
+}
