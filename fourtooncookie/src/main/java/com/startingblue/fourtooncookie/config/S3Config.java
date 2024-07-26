f96bee5cd75bcc684d4936434127e4964cc0529a
JuneBridge <junebridge03@g.skku.edu>
2024-07-26 13:25:30 +0900

    feat: S3Config S3Presigner bean 등록 추가

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/S3Config.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/S3Config.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
@@ -12,6 +13,14 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
                 .region(Region.AP_NORTHEAST_2)
                 .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
