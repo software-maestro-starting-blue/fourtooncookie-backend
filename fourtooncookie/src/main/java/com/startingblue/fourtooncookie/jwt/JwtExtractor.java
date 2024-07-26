db8f127da49245f16aa58577a3ead96678503583
SproutMj <mjk54565@gmail.com>
2024-07-26 17:40:08 +0900

    feat: secret key, issuer 검증하도록 추가

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/jwt/JwtExtractor.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/jwt/JwtExtractor.java
@@ -2,15 +2,28 @@ package com.startingblue.fourtooncookie.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
+@Component
 public class JwtExtractor {
-    private static final Key SECRET_KEY = JwtProvider.getSecretKey();
 
-    public static Claims parseToken(String token) {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.issuer-uri}")
+    private String ISSUER;
+
+    public Claims parseToken(final String token) {
+        final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
         return Jwts.parserBuilder()
-                .setSigningKey(SECRET_KEY)
+                .setSigningKey(key)
+                .requireIssuer(ISSUER)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
