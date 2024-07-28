5d325ba5697aa34389971adf17222508ca378807
JuneBridge <junebridge03@g.skku.edu>
2024-07-28 14:46:40 +0900

    [feat] DallE Vision Service 구현 (#17)
    * init: DallE3VisionApplyService 구조 확립
    
    * refactor & test : Diary, DiaryService, DiaryRepsotiory 테스트 코드 추가  (#14)
    
    * refactor: Diary 필드 NotNull 유효성 검증 추가
    
    * test: Diary, DiaryService, DiaryRepository 테스트 코드 추가
    
    * feat: Gpt4o LLM Service 구현체를 제작하였습니다. (#13)
    
    * feat: Gpt4o LLM Service 구현체를 제작하였습니다.
    
    * fix: 말도 안 되는 이슈 수정
    
    * fix: Rest로 인하여서 발생하는 timeout 에러를 해결하도록 코드를 작성하였습니다.
    
    * chore: 함수명 수정
    
    * chore: 함수명 수정
    
    * chore: import 수정
    
    * feat: 이미지를 4등분한다.
    
    * fix: 이미지 변환 로직에서 try catch가 되도록
    
    * fix: grid별로 이미지를 따로 이벤트 호출하도록 로직을 수정(TSK-210 반영)
    
    * fix: ModelType -> CharacterVisionType 반영
    
    * refactor: Converter 로직을 DallE3VisionApplyService에 반영
    
    * refactor: import 수정
    
    * fix: Event 발생 로직 주석 제거

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/vision/apply/service/DallE3VisionApplyService.java
@@ -0,0 +1,78 @@
+package com.startingblue.fourtooncookie.vision.apply.service;
+
+import com.startingblue.fourtooncookie.character.domain.Character;
+import com.startingblue.fourtooncookie.character.domain.CharacterVisionType;
+import com.startingblue.fourtooncookie.converter.ByteArrayToPngBufferedImageConverter;
+import com.startingblue.fourtooncookie.converter.OneBufferedImageToFourSubImagesConverter;
+import com.startingblue.fourtooncookie.vision.reply.dto.VisionReplyEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.image.*;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+import java.util.Base64;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DallE3VisionApplyService implements VisionApplyService {
+
+    @Value("${prompt.vision.dalle3}")
+    private String SYSTEM_PROMPT;
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    private final OpenAiImageModel openAiImageModel;
+
+    private final ByteArrayToPngBufferedImageConverter byteArrayToPngBufferedImageConverter;
+
+    private final OneBufferedImageToFourSubImagesConverter oneBufferedImageToFourSubImagesConverter;
+
+    @Override
+    public void processVisionApply(Long diaryId, List<String> contents, Character character) {
+        String userPrompt = String.join(" ", contents);
+        String prompt = SYSTEM_PROMPT + userPrompt;
+
+        String imageB64Json = getImageFromDallE3(prompt);
+
+        List<byte[]> imageByteArrays = convertImageB64JsonToFourImagesOfByteArray(imageB64Json);
+
+        for (int i = 0; i < 4; i++) {
+            applicationEventPublisher.publishEvent(new VisionReplyEvent(diaryId, imageByteArrays.get(i), i));
+        }
+    }
+
+    private String getImageFromDallE3(String prompt) {
+        OpenAiImageOptions options = OpenAiImageOptions.builder()
+                .withWidth(1024)
+                .withHeight(1024)
+                .withN(1)
+                .withResponseFormat("b64_json")
+                .withModel("dall-e-3")
+                .withQuality("standard")
+                .build();
+
+        ImageResponse response = openAiImageModel.call(new ImagePrompt(prompt, options));
+
+        return response.getResult().getOutput().getB64Json();
+    }
+
+    private List<byte[]> convertImageB64JsonToFourImagesOfByteArray(String imageB64Json) {
+        byte[] decodedBytes = Base64.getDecoder().decode(imageB64Json);
+
+        BufferedImage image = byteArrayToPngBufferedImageConverter.convertToDatabaseColumn(decodedBytes);
+
+        List<BufferedImage> subImages = oneBufferedImageToFourSubImagesConverter.convertToDatabaseColumn(image);
+
+        return subImages.stream().map(byteArrayToPngBufferedImageConverter::convertToEntityAttribute).toList();
+    }
+
+    @Override
+    public CharacterVisionType getModelType() {
+        return CharacterVisionType.DALL_E_3;
+    }
+}
