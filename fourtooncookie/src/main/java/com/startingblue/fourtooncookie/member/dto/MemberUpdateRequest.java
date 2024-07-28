f3fa67b6264bfedd6db135101535e5e927a38aee
minseo Jo <liging12@naver.com>
2024-07-28 15:32:57 +0900

    refactor: dto 이름 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/dto/MemberUpdateRequest.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/dto/MemberUpdateRequest.java
@@ -3,7 +3,6 @@ package com.startingblue.fourtooncookie.member.dto;
 import com.startingblue.fourtooncookie.member.domain.Gender;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
-public record MemberReadDto(UUID memberId, String email, String name, LocalDate birth, Gender gender) {
+public record MemberUpdateRequest(String name, LocalDate birth, Gender gender) {
 }
