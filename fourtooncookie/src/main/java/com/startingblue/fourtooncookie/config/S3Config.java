fe63e96f1bc04a0f078f1d4bfae98dca49b74e8f
JuneBridge <junebridge03@g.skku.edu>
2024-07-28 14:35:09 +0900

    refactor: region 상수화

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/S3Config.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/S3Config.java
@@ -10,10 +10,12 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
+    private static final Region region = Region.AP_NORTHEAST_2;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.AP_NORTHEAST_2)
+                .region(region)
                 .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
     }
@@ -21,7 +23,7 @@ public class S3Config {
     @Bean
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
-                .region(Region.AP_NORTHEAST_2)
+                .region(region)
                 .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
     }
