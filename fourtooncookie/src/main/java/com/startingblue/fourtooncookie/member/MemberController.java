b5f020b9d5d7a3817484bd42b2dba1080909ba62
minseo Jo <liging12@naver.com>
2024-07-28 15:34:02 +0900

    feat: MemberService update, softDelete 구현

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/MemberController.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/MemberController.java
@@ -1,16 +1,13 @@
 package com.startingblue.fourtooncookie.member;
 
-import com.startingblue.fourtooncookie.member.dto.MemberReadDto;
-import com.startingblue.fourtooncookie.member.dto.MemberUpdateDto;
+import com.startingblue.fourtooncookie.member.dto.MemberSavedResponse;
+import com.startingblue.fourtooncookie.member.dto.MemberUpdateRequest;
 import com.startingblue.fourtooncookie.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -20,22 +17,23 @@ public final class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/member/{memberId}")
-    public ResponseEntity<MemberReadDto> readMember(@PathVariable final UUID memberId) {
-        MemberReadDto memberReadDto = memberService.readById(memberId);
-        return ResponseEntity.ok(memberReadDto);
+    public ResponseEntity<MemberSavedResponse> readMember(@PathVariable final UUID memberId) {
+        MemberSavedResponse memberSavedResponse = memberService.readById(memberId);
+        return ResponseEntity.ok(memberSavedResponse);
     }
 
-    @PutMapping("/member/{memberId}")
-    public ResponseEntity<Void> updateMember(@PathVariable final UUID memberId, final MemberUpdateDto memberUpdateDto) {
-        memberService.updateById(memberId, memberUpdateDto);
+    @PatchMapping("/member/{memberId}")
+    public ResponseEntity<Void> updateMember(@PathVariable final UUID memberId, final MemberUpdateRequest memberUpdateRequest) {
+        memberService.updateById(memberId, memberUpdateRequest);
         return ResponseEntity
                 .noContent()
                 .build();
     }
 
     @DeleteMapping("/member/{memberId}")
-    public ResponseEntity<Void> deleteMember(@PathVariable final UUID memberId) {
-        memberService.deleteById(memberId);
+    public ResponseEntity<Void> softDeleteMember(@PathVariable final UUID memberId) {
+        LocalDateTime deleteLocalDateTime = LocalDateTime.now();
+        memberService.softDeleteById(memberId, deleteLocalDateTime);
         return ResponseEntity
                 .noContent()
                 .build();
