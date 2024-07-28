230e8e48dc437d9f9f091b744102a9e146c5b9e4
minseo Jo <liging12@naver.com>
2024-07-28 16:01:38 +0900

    refactor: 인증 인터셉터 -> 인증 필터로 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/authentication/AuthenticationFilter.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/authentication/AuthenticationFilter.java
@@ -3,40 +3,40 @@ package com.startingblue.fourtooncookie.config.authentication;
 import com.startingblue.fourtooncookie.jwt.JwtExtractor;
 import com.startingblue.fourtooncookie.member.domain.MemberRepository;
 import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.util.Objects;
+import java.io.IOException;
 import java.util.UUID;
-
 
 @RequiredArgsConstructor
 @Component
 @Slf4j
-public class AuthenticationInterceptor implements HandlerInterceptor {
+public class AuthenticationFilter extends HttpFilter {
 
     private final JwtExtractor jwtExtractor;
     private final MemberRepository memberRepository;
 
     @Override
-    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) {
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         final String token = jwtExtractor.resolveToken(request);
-        if (!token.isEmpty()) {
+        if (token != null && !token.isEmpty()) {
             final Claims claims = jwtExtractor.parseToken(token);
             final UUID memberId = UUID.fromString(claims.getSubject());
 
             boolean existsById = memberRepository.existsById(memberId);
             if (existsById) {
                 request.setAttribute("memberId", memberId);
-                return true;
+                chain.doFilter(request, response);
+                return;
             }
         }
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        return false;
     }
 }
