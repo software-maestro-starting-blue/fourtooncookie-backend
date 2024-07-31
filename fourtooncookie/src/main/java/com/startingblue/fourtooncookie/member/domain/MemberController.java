2be0836bfe0cfc0e0e41b6b8097d507267d6b6e2
minseo Jo <liging12@naver.com>
2024-07-31 13:56:42 +0900

    feat: MemberCotnroller

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/domain/MemberController.java
@@ -0,0 +1,41 @@
+package com.startingblue.fourtooncookie.member.domain;
+
+import com.startingblue.fourtooncookie.member.dto.MemberSavedResponse;
+import com.startingblue.fourtooncookie.member.dto.MemberUpdateRequest;
+import com.startingblue.fourtooncookie.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public final class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/member/{memberId}")
+    public ResponseEntity<MemberSavedResponse> readMember(@PathVariable final UUID memberId) {
+        MemberSavedResponse memberSavedResponse = memberService.readById(memberId);
+        return ResponseEntity.ok(memberSavedResponse);
+    }
+
+    @PatchMapping("/member/{memberId}")
+    public ResponseEntity<Void> updateMember(@PathVariable final UUID memberId, final MemberUpdateRequest memberUpdateRequest) {
+        memberService.updateById(memberId, memberUpdateRequest);
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+
+    @DeleteMapping("/member/{memberId}")
+    public ResponseEntity<Void> softDeleteMember(@PathVariable final UUID memberId) {
+        LocalDateTime deleteLocalDateTime = LocalDateTime.now();
+        memberService.softDeleteById(memberId, deleteLocalDateTime);
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+}
