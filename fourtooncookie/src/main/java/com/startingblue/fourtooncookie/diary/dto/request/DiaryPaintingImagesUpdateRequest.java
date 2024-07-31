d3c16edd76197feddce625740a809626621febd6
minseo Jo <liging12@naver.com>
2024-08-01 07:16:06 +0900

    refactor: Diary 관련 DTO

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiaryPaintingImagesUpdateRequest.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiaryPaintingImagesUpdateRequest.java
@@ -1,7 +1,13 @@
 package com.startingblue.fourtooncookie.diary.dto.request;
 
+import com.startingblue.fourtooncookie.validator.NotEmptyList;
+import jakarta.validation.constraints.Size;
+
 import java.net.URL;
 import java.util.List;
 
-public record DiaryPaintingImagesUpdateRequest(List<URL> paintingImageUrls){
+public record DiaryPaintingImagesUpdateRequest(
+        @NotEmptyList(message = "일기 그림 URL 목록은 최소 1개를 포함해야 합니다.")
+        @Size(min = 1, max = 4, message = "일기 그림 URL 목록은 1개에서 4개 사이여야 합니다.")
+        List<URL> paintingImageUrls){
 }
--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiaryUpdateRequest.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiaryUpdateRequest.java
@@ -1,6 +1,20 @@
 package com.startingblue.fourtooncookie.diary.dto.request;
 
+import com.startingblue.fourtooncookie.validator.NotEmptyList;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
-public record DiaryUpdateRequest(String content, List<Long> hashtagIds, Long characterId) {
+public record DiaryUpdateRequest(
+        @NotBlank(message = "일기 내용은 필수 입니다.")
+        @Size(min = 1, max = 1000, message = "일기 내용은 1자 이상 1000자 이내여야 합니다.")
+        String content,
+
+        @NotEmptyList(message = "해시태그 ID 목록은 최소 1개를 포함해야 합니다.")
+        @Size(min = 1, max = 4, message = "해시태그 ID 목록은 1개에서 4개 사이여야 합니다.")
+        List<Long> hashtagIds,
+
+        @NotNull(message = "일기에 그려질 캐릭터 ID는 필수 입니다.") Long characterId) {
 }
