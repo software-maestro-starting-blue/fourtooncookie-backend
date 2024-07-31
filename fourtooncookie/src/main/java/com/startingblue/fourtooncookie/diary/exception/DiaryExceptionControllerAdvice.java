eb0a1554a94cea7ded1c483396d5e58349a22a9f
minseo Jo <liging12@naver.com>
2024-08-01 07:15:46 +0900

    refactor: Diary 관련 Excpetion

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/exception/DiaryExceptionControllerAdvice.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/exception/DiaryExceptionControllerAdvice.java
@@ -1,27 +1,43 @@
 package com.startingblue.fourtooncookie.diary.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice("com.startingblue.fourtooncookie.diary")
 public class DiaryExceptionControllerAdvice {
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleConstraintViolationException(ConstraintViolationException e) {
+        return "Diary constraint violation " + e.getMessage();
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Diary not found: " + e.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleEntityNotFoundException(EntityNotFoundException e) {
+        return "Diary not found: " + e.getMessage();
     }
 
-    @ExceptionHandler(DiaryNoSuchElementException.class)
-    public ResponseEntity<String> handleDiaryNoSuchElementException(DiaryNoSuchElementException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Diary not found: " + e.getMessage());
+    @ExceptionHandler(DiaryNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleDiaryNotFoundException(DiaryNotFoundException e) {
+        return "Diary not found: " + e.getMessage();
     }
 
-
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleDiaryNoSuchElementException(RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Diary default image path not found:  " + e.getMessage());
+    @ExceptionHandler(DiaryDuplicateException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleDiaryDuplicateException(DiaryDuplicateException e) {
+        return "Diary duplicate: " + e.getMessage();
     }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
+
 }
