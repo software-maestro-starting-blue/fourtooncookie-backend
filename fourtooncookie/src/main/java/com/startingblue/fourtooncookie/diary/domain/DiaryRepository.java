d139140a3ce026b8d2ffd916aed799065fe48110
minseo Jo <liging12@naver.com>
2024-08-01 07:17:11 +0900

    feat: DiaryRepository, 중복 일기 체크

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/domain/DiaryRepository.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/domain/DiaryRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     Page<Diary> findAllByMemberIdOrderByDiaryDateDesc(UUID memberId, Pageable pageable);
+    boolean existsByMemberIdAndDiaryDate(UUID memberId, LocalDate diaryDate);
 }
