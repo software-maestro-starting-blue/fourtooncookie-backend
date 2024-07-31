5ef0a276da876f98e8dd0b94ba58aa9baf11ad80
minseo Jo <liging12@naver.com>
2024-08-01 07:16:22 +0900

    Refactor: DiaryController

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/DiaryController.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/DiaryController.java
@@ -3,18 +3,20 @@ package com.startingblue.fourtooncookie.diary;
 import com.startingblue.fourtooncookie.diary.dto.request.DiaryPaintingImagesUpdateRequest;
 import com.startingblue.fourtooncookie.diary.dto.request.DiarySaveRequest;
 import com.startingblue.fourtooncookie.diary.dto.request.DiaryUpdateRequest;
-import com.startingblue.fourtooncookie.diary.dto.response.DiarySavedResponse;
+import com.startingblue.fourtooncookie.diary.dto.response.DiarySavedResponses;
 import com.startingblue.fourtooncookie.diary.service.DiaryService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
+
+import static org.springframework.http.ResponseEntity.*;
 
 @RestController
 @RequestMapping("/diary")
@@ -25,47 +27,52 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @PostMapping
-    public ResponseEntity<Void> createDiary(@RequestBody final DiarySaveRequest request) {
+    public ResponseEntity<HttpStatus> createDiary(@RequestBody final DiarySaveRequest request) {
         diaryService.createDiary(request, UUID.randomUUID()); // TODO : 우선 디폴트 값 넣어 놓음.
-        return ResponseEntity.ok().build();
+        return status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/timeline")
-    public ResponseEntity<List<DiarySavedResponse>> readDiariesByMember (
+    public ResponseEntity<DiarySavedResponses> readDiariesByMember (
             HttpServletRequest httpRequest,
             @RequestParam(defaultValue = "0") @Min(0) @Max(200) final int pageNumber,
             @RequestParam(defaultValue = "10") @Min(1) @Max(10) final int pageSize) {
+
         UUID memberId = UUID.fromString(httpRequest.getHeader("memberId")); // TODO 현재는 헤더에 넣고, jwt 를 이용코드로 변경 예정
-        List<DiarySavedResponse> responses = diaryService.readDiariesByMember(memberId, pageNumber, pageSize);
-        if (responses.isEmpty()) {
-            return ResponseEntity.noContent().build();
+        DiarySavedResponses responses = DiarySavedResponses.of(diaryService.readDiariesByMember(memberId, pageNumber, pageSize));
+        if (responses.diarySavedResponses().isEmpty()) {
+            return noContent().build();
         }
-        return ResponseEntity.ok(responses);
+        return ok(responses);
     }
 
-    @PatchMapping("/{diaryId}")
-    public ResponseEntity<Void> updateDiary(@PathVariable final Long diaryId,
+    @PutMapping("/{diaryId}")
+    public ResponseEntity<HttpStatus> updateDiary(@PathVariable final Long diaryId,
                                             @RequestBody final DiaryUpdateRequest request) {
+
         diaryService.updateDiary(diaryId, request);
-        return ResponseEntity.ok().build();
+        return ok().build();
+    }
+
+    @PatchMapping("/{diaryId}/favorite")
+    public ResponseEntity<HttpStatus> toggleDiaryFavorite(@PathVariable final Long diaryId,
+                                                    @RequestBody final boolean isFavorite) {
+
+        diaryService.toggleFavoriteDiary(diaryId, isFavorite);
+        return ok().build();
     }
 
     @PatchMapping("/{diaryId}/painting-images")
-    public ResponseEntity<Void> updateDiary(@PathVariable final Long diaryId,
+    public ResponseEntity<HttpStatus> updateDiaryPaintingImages(@PathVariable final Long diaryId,
                                             @RequestBody final DiaryPaintingImagesUpdateRequest request) {
+
         diaryService.updateDiary(diaryId, request);
-        return ResponseEntity.ok().build();
+        return ok().build();
     }
 
     @DeleteMapping("/{diaryId}")
-    public ResponseEntity<Void> deleteDiary(@PathVariable final Long diaryId) {
+    public ResponseEntity<HttpStatus> deleteDiary(@PathVariable final Long diaryId) {
         diaryService.deleteDiary(diaryId);
-        return ResponseEntity.noContent().build();
-    }
-
-    @PatchMapping("/favorite/{diaryId}")
-    public ResponseEntity<Void> favoriteDiary(@PathVariable final Long diaryId, @RequestBody final boolean isFavorite) {
-        log.info("favorite: {} diary {}", isFavorite, diaryId);
-        return ResponseEntity.ok().build();
+        return noContent().build();
     }
 }
