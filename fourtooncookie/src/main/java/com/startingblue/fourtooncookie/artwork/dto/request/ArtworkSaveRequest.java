22becfc0c2161cda4f7f68eab825125cdc0a800a
minseo Jo <liging12@naver.com>
2024-07-31 22:00:56 +0900

    refactor: 메시지 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/dto/request/ArtworkSaveRequest.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/dto/request/ArtworkSaveRequest.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.Size;
 import java.net.URL;
 
 public record ArtworkSaveRequest(
-        @Size(min = 1, max = 255, message =  "작품명은 비워둘 수 없습니다.") String title,
+        @Size(min = 1, max = 255, message = "작품명의 글자수는 1에서 255자 이내여야 합니다.") String title,
         @NotNull(message = "작품 썸네일 URL이 존재해야 합니다.") URL thumbnailUrl) {
 }
--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/dto/request/ArtworkUpdateRequest.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/dto/request/ArtworkUpdateRequest.java
@@ -1,12 +1,11 @@
 package com.startingblue.fourtooncookie.artwork.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.net.URL;
 
 public record ArtworkUpdateRequest(
-        @NotBlank @Size(min = 1, max = 255) String title,
+        @Size(min = 1, max = 255, message = "작품명의 글자수는 1에서 255자 이내여야 합니다.") String title,
         @NotNull(message = "작품 썸네일 URL이 존재해야 합니다.") URL thumbnailUrl) {
 }
