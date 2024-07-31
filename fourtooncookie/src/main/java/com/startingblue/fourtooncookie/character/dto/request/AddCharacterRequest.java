6d36eb5e8e554086a14c87b9b37ef39ce20e3042
minseo Jo <liging12@naver.com>
2024-08-01 04:33:57 +0900

    refactor: DTO 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/dto/request/AddCharacterRequest.java
+++
@@ -1,8 +0,0 @@
-package com.startingblue.fourtooncookie.character.dto.request;
-
-import com.startingblue.fourtooncookie.character.domain.PaymentType;
-
-import java.net.URL;
-
-public record AddCharacterRequest(String characterVisionType, PaymentType paymentType, Long artworkId, String name, URL selectionThumbnailUrl, String basePrompt) {
-}
