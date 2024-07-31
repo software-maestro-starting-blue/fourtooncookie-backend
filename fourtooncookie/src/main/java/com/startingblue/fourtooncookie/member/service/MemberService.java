6dd3e007d9902f1eebca338605e61facba6dfcd4
minseo Jo <liging12@naver.com>
2024-07-31 13:57:00 +0900

    feat : MemberService

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/service/MemberService.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/service/MemberService.java
@@ -2,10 +2,13 @@ package com.startingblue.fourtooncookie.member.service;
 
 import com.startingblue.fourtooncookie.member.domain.Member;
 import com.startingblue.fourtooncookie.member.domain.MemberRepository;
+import com.startingblue.fourtooncookie.member.dto.MemberSavedResponse;
+import com.startingblue.fourtooncookie.member.dto.MemberUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -24,6 +27,12 @@ public class MemberService {
         memberRepository.save(member);
     }
 
+    public MemberSavedResponse readById(UUID memberId) {
+        Member member = findById(memberId);
+        return MemberSavedResponse.of(member.getEmail(), member.getName(), member.getGender(), member.getBirth());
+    }
+
+
     public void verifyMemberExists(UUID memberId) {
         boolean existsById = memberRepository.existsById(memberId);
         if (!existsById) {
@@ -31,4 +40,20 @@ public class MemberService {
         }
     }
 
+    public void updateById(final UUID memberId, final MemberUpdateRequest memberUpdateRequest) {
+        Member member = findMemberOrThrow(memberId);
+        member.update(memberUpdateRequest.name(), memberUpdateRequest.birth(), memberUpdateRequest.gender());
+        memberRepository.save(member);
+    }
+
+    public void softDeleteById(UUID memberId, LocalDateTime current) {
+        Member foundMember = findMemberOrThrow(memberId);
+        foundMember.softDelete(current);
+        memberRepository.save(foundMember);
+    }
+
+    public Member findMemberOrThrow(UUID memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("Member with ID " + memberId + " not found."));
+    }
 }
