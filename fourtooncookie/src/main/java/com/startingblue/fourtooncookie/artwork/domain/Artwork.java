ebb769251a16a329f7a2888ab2847321933e4569
minseo Jo <liging12@naver.com>
2024-07-31 17:20:33 +0900

    refactor: Artwork 도메인
    - @AllArgsConstructor(access = AccessLevel.PRIVATE)를 통해
    Artwork(id, title, thumbnail) 생성자 방지
    
    - 검증 예외 메시지 추가

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/domain/Artwork.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/domain/Artwork.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,17 +16,20 @@ import java.net.URL;
 @Slf4j
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Artwork {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "artwork_id")
     private Long id;
 
-    @NotBlank
-    @Size(max = 255)
+    @NotBlank(message = "작품명은 비워둘 수 없습니다.")
+    @Size(min = 1, max = 255, message = "작품명의 글자수는 1에서 255자 이내여야 합니다.")
     private String title;
 
-    @NotNull
+    @NotNull(message = "작품 썸네일이 존재해야 합니다.")
+    @Column(name = "thumbnail_url", nullable = false)
     private URL thumbnailUrl;
 
     public Artwork(final String title, final URL thumbnailUrl) {
