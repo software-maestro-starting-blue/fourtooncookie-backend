707a789639fffedabecd0aa323b1d116de2ebbcd
minseo Jo <liging12@naver.com>
2024-08-01 20:34:14 +0900

    feat: converterException

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/exception/ConverterExceptionControllerAdvice.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/exception/ConverterExceptionControllerAdvice.java
@@ -1,15 +1,28 @@
 package com.startingblue.fourtooncookie.converter.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice("com.startingblue.fourtooncookie.converter")
 public class ConverterExceptionControllerAdvice {
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleIllegalArgumentException(IllegalArgumentException e) {
+        return e.getMessage();
+    }
+
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeExceptionException(RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("converter exception" + e.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleRuntimeExceptionException(RuntimeException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception e) {
+        return e.getMessage();
     }
 }
