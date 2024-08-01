f8f7ed1e263cbb73ced893dc66c9eea08664de3e
minseo Jo <liging12@naver.com>
2024-08-01 19:19:37 +0900

    feat: 해시태그 추가

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/hashtag/domain/Hashtag.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/hashtag/domain/Hashtag.java
@@ -1,6 +1,6 @@
 package com.startingblue.fourtooncookie.hashtag.domain;
 
-import com.startingblue.fourtooncookie.hashtag.exception.HashtagNoSuchElementException;
+import com.startingblue.fourtooncookie.hashtag.exception.HashtagNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,9 +10,41 @@ import java.util.*;
 @AllArgsConstructor
 public enum Hashtag {
 
-    SAD(1L, "슬픔", HashtagType.EMOTION),
-    DSAS(2L, "슬픔", HashtagType.EMOTION);
-    // TODO : 해시 태그 종류 및 아이디 관리는 회의 필요
+    // Weather
+    CLEAR(1L, "맑음", HashtagType.WEATHER),
+    CLOUDY(2L, "흐림", HashtagType.WEATHER),
+    RAIN(3L, "비", HashtagType.WEATHER),
+    SNOW(4L, "눈", HashtagType.WEATHER),
+    WINDY(5L, "바람", HashtagType.WEATHER),
+    YELLOW_DUST(6L, "황사", HashtagType.WEATHER),
+    HEATWAVE(7L, "폭염", HashtagType.WEATHER),
+    COLD_WAVE(8L, "한파", HashtagType.WEATHER),
+    TYPHOON(9L, "태풍", HashtagType.WEATHER),
+
+    // Emotions
+    EXCITED(100L, "신나는", HashtagType.EMOTION),
+    RELAXED(101L, "편안한", HashtagType.EMOTION),
+    PROUD(102L, "뿌듯한", HashtagType.EMOTION),
+    ANTICIPATING(103L, "기대되는", HashtagType.EMOTION),
+    HAPPY(104L, "행복한", HashtagType.EMOTION),
+    MOTIVATED(105L, "의욕적인", HashtagType.EMOTION),
+    THRILLED(106L, "설레는", HashtagType.EMOTION),
+    REFRESHING(107L, "상쾌한", HashtagType.EMOTION),
+    DEPRESSED(108L, "우울한", HashtagType.EMOTION),
+    LONELY(109L, "외로운", HashtagType.EMOTION),
+    ANXIOUS(110L, "불안한", HashtagType.EMOTION),
+    SAD(111L, "슬픈", HashtagType.EMOTION),
+    ANGRY(112L, "화난", HashtagType.EMOTION),
+    PRESSURED(113L, "부담되는", HashtagType.EMOTION),
+    ANNOYED(114L, "짜증나는", HashtagType.EMOTION),
+    TIRED(115L, "피곤한", HashtagType.EMOTION),
+
+    // Met Person
+    FRIEND(200L, "친구", HashtagType.MET_PERSON),
+    FAMILY(201L, "가족", HashtagType.MET_PERSON),
+    LOVER(202L, "애인", HashtagType.MET_PERSON),
+    ACQUAINTANCE(203L, "지인", HashtagType.MET_PERSON),
+    NO_ONE(204L, "안만남", HashtagType.MET_PERSON);
 
     private final Long id;
     private final String name;
@@ -27,12 +59,12 @@ public enum Hashtag {
         return result;
     }
 
-    private static Hashtag findHashtagById(Long id) {
+    public static Hashtag findHashtagById(Long id) {
         for (Hashtag hashtag : Hashtag.values()) {
             if (hashtag.getId().equals(id)) {
                 return hashtag;
             }
         }
-        throw new HashtagNoSuchElementException();
+        throw new HashtagNotFoundException();
     }
 }
