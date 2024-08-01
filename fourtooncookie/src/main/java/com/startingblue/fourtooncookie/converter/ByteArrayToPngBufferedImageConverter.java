e77d32cedb5c32963a1cdaa457dcb5ee9316b83b
minseo Jo <liging12@naver.com>
2024-08-01 20:35:27 +0900

    refactor: ByteArrayToPngBufferedImageConverter

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/ByteArrayToPngBufferedImageConverter.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/ByteArrayToPngBufferedImageConverter.java
@@ -1,5 +1,6 @@
 package com.startingblue.fourtooncookie.converter;
 
+import com.startingblue.fourtooncookie.converter.exception.ConversionException;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import org.springframework.stereotype.Component;
@@ -13,25 +14,38 @@ import java.io.ByteArrayOutputStream;
 @Component
 public class ByteArrayToPngBufferedImageConverter implements AttributeConverter<byte[], BufferedImage> {
 
-
     @Override
     public BufferedImage convertToDatabaseColumn(byte[] attribute) {
-        ByteArrayInputStream bais = new ByteArrayInputStream(attribute);
-        try {
-            return ImageIO.read(bais);
-        } catch (Exception e) {
-            throw new RuntimeException("Error converting byte array to BufferedImage", e);
+        if (attribute == null) {
+            throw new ConversionException("Byte array attribute is null", null);
         }
+        return convertByteArrayToBufferedImage(attribute);
     }
 
     @Override
     public byte[] convertToEntityAttribute(BufferedImage dbData) {
+        if (dbData == null) {
+            throw new ConversionException("BufferedImage attribute is null", null);
+        }
+        return convertBufferedImageToByteArray(dbData);
+    }
+
+    private BufferedImage convertByteArrayToBufferedImage(byte[] attribute) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(attribute);
+        try {
+            return ImageIO.read(bais);
+        } catch (Exception e) {
+            throw new ConversionException("Error converting byte array to BufferedImage", e);
+        }
+    }
+
+    private byte[] convertBufferedImageToByteArray(BufferedImage dbData) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
             ImageIO.write(dbData, "png", baos);
             return baos.toByteArray();
         } catch (Exception e) {
-            throw new RuntimeException("Error converting BufferedImage to byte array", e);
+            throw new ConversionException("Error converting BufferedImage to byte array", e);
         }
     }
 }
