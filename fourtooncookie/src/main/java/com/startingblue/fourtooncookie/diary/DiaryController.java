0905cb53d4cf6a2a764d08cb1a568969377a2578
minseo Jo <liging12@naver.com>
2024-08-05 00:36:26 +0900

    refactor: diaryService - MemberId 가져오기

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/DiaryController.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/DiaryController.java
@@ -4,7 +4,6 @@ import com.startingblue.fourtooncookie.diary.dto.request.DiarySaveRequest;
 import com.startingblue.fourtooncookie.diary.dto.request.DiaryUpdateRequest;
 import com.startingblue.fourtooncookie.diary.dto.response.DiarySavedResponses;
 import com.startingblue.fourtooncookie.diary.service.DiaryService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
@@ -26,18 +25,19 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @PostMapping
-    public ResponseEntity<HttpStatus> createDiary(@RequestBody final DiarySaveRequest request) {
-        diaryService.createDiary(request, UUID.randomUUID()); // TODO : 우선 디폴트 값 넣어 놓음.
+    public ResponseEntity<HttpStatus> createDiary(UUID memberId,
+                                                  @RequestBody final DiarySaveRequest request) {
+        diaryService.createDiary(request, memberId); // TODO : 우선 디폴트 값 넣어 놓음.
         return status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/timeline")
     public ResponseEntity<DiarySavedResponses> readDiariesByMember (
-            HttpServletRequest httpRequest,
+            UUID memberId,
             @RequestParam(defaultValue = "0") @Min(0) @Max(200) final int pageNumber,
             @RequestParam(defaultValue = "10") @Min(1) @Max(10) final int pageSize) {
-        UUID memberId = UUID.fromString(httpRequest.getHeader("memberId")); // TODO 현재는 헤더에 넣고, jwt 를 이용코드로 변경 예정
         DiarySavedResponses responses = DiarySavedResponses.of(diaryService.readDiariesByMemberId(memberId, pageNumber, pageSize));
+
         if (responses.diarySavedResponses().isEmpty()) {
             return noContent().build();
         }
@@ -47,7 +47,6 @@ public class DiaryController {
     @PutMapping("/{diaryId}")
     public ResponseEntity<HttpStatus> updateDiary(@PathVariable final Long diaryId,
                                             @RequestBody final DiaryUpdateRequest request) {
-
         diaryService.updateDiary(diaryId, request);
         return ok().build();
     }
