de21161dcff623adad8377b4b8842ad6896a6239
minseo Jo <liging12@naver.com>
2024-08-05 05:47:44 +0900

    refactor: AuthenticationFilter

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/authentication/AuthenticationFilter.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/authentication/AuthenticationFilter.java
@@ -1,9 +1,12 @@
 package com.startingblue.fourtooncookie.config.authentication;
 
+import com.startingblue.fourtooncookie.artwork.exception.ArtworkNotFoundException;
+import com.startingblue.fourtooncookie.character.exception.CharacterNotFoundException;
 import com.startingblue.fourtooncookie.jwt.JwtExtractor;
 import com.startingblue.fourtooncookie.member.service.MemberService;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,34 +24,26 @@ public class AuthenticationFilter extends HttpFilter {
 
     private final JwtExtractor jwtExtractor;
     private final MemberService memberService;
-    private final AuthenticationExceptionHandler exceptionHandler;
 
     @Override
-    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException {
-        try {
-            String requestURI = request.getRequestURI();
-            if (requestURI.startsWith("/h2-console")) {
-                chain.doFilter(request, response);
-                return;
-            }
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+                                                                        throws IOException, ServletException {
+        String requestURI = request.getRequestURI();
+        if (requestURI.startsWith("/h2-console")) {
+            chain.doFilter(request, response);
+            return;
+        }
 
-            String token = jwtExtractor.resolveToken(request);
-            Claims claims = jwtExtractor.parseToken(token);
-            UUID memberId = UUID.fromString(claims.getSubject());
-            log.info("login attempt memberId: {}", memberId);
-            if (memberService.verifyMemberExists(memberId)) {
-                log.info("login success memberId: {}", memberId);
-                request.setAttribute("memberId", memberId);
-                System.out.println(memberId);
-                chain.doFilter(request, response);
-            } else {
-                response.sendError(HttpServletResponse.SC_NOT_FOUND, String.format("Member with id %s not found", memberId));
-            }
-        } catch (AuthenticationException e) {
-            exceptionHandler.handleAuthenticationException(e, response);
-        } catch (Exception e) {
-            exceptionHandler.handleGeneralException(e, response);
+        String token = jwtExtractor.resolveToken(request);
+        Claims claims = jwtExtractor.parseToken(token);
+        UUID memberId = UUID.fromString(claims.getSubject());
+        log.info("login attempt memberId: {}", memberId);
+        if (memberService.verifyMemberExists(memberId)) {
+            log.info("login success memberId: {}", memberId);
+            request.setAttribute("memberId", memberId);
+            chain.doFilter(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, String.format("Member with id %s not found", memberId));
         }
     }
-
 }
