9422a0771caee74e9f02fcca01f59aab523bd7e6
minseo Jo <liging12@naver.com>
2024-08-04 14:57:09 +0900

    refactor: throwable 추가

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/hashtag/exception/HashtagExceptionControllerAdvice.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/hashtag/exception/HashtagExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.startingblue.fourtooncookie.hashtag.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -8,21 +9,20 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice("com.startingblue.fourtooncookie.hashtag")
+@Slf4j
 public class HashtagExceptionControllerAdvice {
-
-    private static final Logger log = LoggerFactory.getLogger(HashtagExceptionControllerAdvice.class);
 
     @ExceptionHandler(HashtagNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleHashtagNotFoundException(HashtagNotFoundException e) {
-        log.error(e.getMessage());
+        log.error(e.getMessage(), e);
         return "Hashtag not found";
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleIllegalArgumentException(IllegalArgumentException e) {
-        log.error(e.getMessage());
+        log.error(e.getMessage(), e);
         return "Bad Request";
     }
 }
