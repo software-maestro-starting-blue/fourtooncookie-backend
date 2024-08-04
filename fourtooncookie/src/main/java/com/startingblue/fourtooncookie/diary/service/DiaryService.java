42eb51f26b1326320c6e8ab994deb3a939c8f7ea
minseo Jo <liging12@naver.com>
2024-08-05 05:47:32 +0900

    fix: hashtag not found

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/service/DiaryService.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/service/DiaryService.java
@@ -8,6 +8,7 @@ import com.startingblue.fourtooncookie.diary.dto.request.DiarySaveRequest;
 import com.startingblue.fourtooncookie.diary.dto.request.DiaryUpdateRequest;
 import com.startingblue.fourtooncookie.diary.exception.DiaryDuplicateException;
 import com.startingblue.fourtooncookie.diary.exception.DiaryNotFoundException;
+import com.startingblue.fourtooncookie.hashtag.domain.Hashtag;
 import com.startingblue.fourtooncookie.member.domain.Member;
 import com.startingblue.fourtooncookie.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -49,6 +50,9 @@ public class DiaryService {
     public void createDiary(final DiarySaveRequest request, final UUID memberId) {
         Member member = memberService.readById(memberId);
         Character character = characterService.readById(request.characterId());
+        List<Long> hashtagIds = Hashtag.findHashtagIdsByIds(request.hashtagIds())
+                .stream()
+                .toList();
 
         verifyUniqueDiary(memberId, request.diaryDate());
 
@@ -56,7 +60,7 @@ public class DiaryService {
                 .content(request.content())
                 .isFavorite(false)
                 .diaryDate(request.diaryDate())
-                .hashtagsIds(request.hashtagIds())
+                .hashtagsIds(hashtagIds)
                 .paintingImageUrls(DIARY_DEFAULT_IMAGE_URLS)
                 .character(character)
                 .memberId(member.getId())
