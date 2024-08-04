2071c58169f9111b35f72a8bf64dc0f713ea11c4
minseo Jo <liging12@naver.com>
2024-08-04 21:31:03 +0900

    refactor: 코드리뷰 반영

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/OneBufferedImageToFourSubImagesConverter.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/OneBufferedImageToFourSubImagesConverter.java
@@ -19,23 +19,19 @@ public class OneBufferedImageToFourSubImagesConverter {
         }
 
         try {
-            return extractSubImages(image);
+            int width = image.getWidth();
+            int height = image.getHeight();
+            List<BufferedImage> subImages = new ArrayList<>();
+
+            subImages.add(image.getSubimage(0, 0, width / 2, height / 2));                  // top-left
+            subImages.add(image.getSubimage(width / 2, 0, width / 2, height / 2));          // top-right
+            subImages.add(image.getSubimage(0, height / 2, width / 2, height / 2));         // bottom-left
+            subImages.add(image.getSubimage(width / 2, height / 2, width / 2, height / 2)); // bottom-right
+
+            return subImages;
         } catch (Exception e) {
             throw new ConversionException("Error splitting BufferedImage into subimages", e);
         }
-    }
-
-    private List<BufferedImage> extractSubImages(BufferedImage image) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-        List<BufferedImage> subImages = new ArrayList<>();
-
-        subImages.add(image.getSubimage(0, 0, width / 2, height / 2));                  // top-left
-        subImages.add(image.getSubimage(width / 2, 0, width / 2, height / 2));          // top-right
-        subImages.add(image.getSubimage(0, height / 2, width / 2, height / 2));         // bottom-left
-        subImages.add(image.getSubimage(width / 2, height / 2, width / 2, height / 2)); // bottom-right
-
-        return subImages;
     }
 
     public BufferedImage combineSubImagesToImage(List<BufferedImage> subImages) {
@@ -43,27 +39,20 @@ public class OneBufferedImageToFourSubImagesConverter {
             throw new ConversionException("List<BufferedImage> dbData is null or does not contain exactly 4 subimages", null);
         }
 
-        try {
-            return reconstructImage(subImages);
-        } catch (Exception e) {
-            throw new ConversionException("Error reconstructing BufferedImage from subimages", e);
-        }
-    }
-
-    private BufferedImage reconstructImage(List<BufferedImage> subImages) {
         int subImageWidth = subImages.get(0).getWidth();
         int subImageHeight = subImages.get(0).getHeight();
         int originalWidth = subImageWidth * 2;
         int originalHeight = subImageHeight * 2;
+        try {
+            BufferedImage reconstructedImage = new BufferedImage(originalWidth, originalHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = reconstructedImage.createGraphics();
 
-        BufferedImage reconstructedImage = new BufferedImage(originalWidth, originalHeight, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = reconstructedImage.createGraphics();
-
-        drawSubImages(reconstructedImage, g2d, subImages, subImageWidth, subImageHeight);
-
-        g2d.dispose();
-
-        return reconstructedImage;
+            drawSubImages(reconstructedImage, g2d, subImages, subImageWidth, subImageHeight);
+            g2d.dispose();
+            return reconstructedImage;
+        } catch (Exception e) {
+            throw new ConversionException("Error reconstructing BufferedImage from subimages", e);
+        }
     }
 
     private void drawSubImages(BufferedImage reconstructedImage, Graphics2D g2d, List<BufferedImage> subImages, int subImageWidth, int subImageHeight) {
