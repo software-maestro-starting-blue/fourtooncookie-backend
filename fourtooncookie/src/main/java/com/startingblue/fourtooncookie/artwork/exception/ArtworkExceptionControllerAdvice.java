96eafd1ef6c67626569fa86f79127d6ad08a1e14
minseo Jo <liging12@naver.com>
2024-08-04 14:51:00 +0900

    refactor: 예외 메시지 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/exception/ArtworkExceptionControllerAdvice.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/exception/ArtworkExceptionControllerAdvice.java
@@ -2,6 +2,8 @@ package com.startingblue.fourtooncookie.artwork.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,34 +12,41 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice("com.startingblue.fourtooncookie.artwork")
 public class ArtworkExceptionControllerAdvice {
 
+    private static final Logger log = LoggerFactory.getLogger(ArtworkExceptionControllerAdvice.class);
+
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleConstraintViolationException(ConstraintViolationException e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Artwork Constraint Violation";
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleEntityNotFoundException(EntityNotFoundException e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Artwork not found";
     }
 
     @ExceptionHandler(ArtworkNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleArtworkNotFoundException(ArtworkNotFoundException e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Artwork not found";
     }
 
     @ExceptionHandler(ArtworkDuplicateException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public String handleArtworkDuplicateException(ArtworkDuplicateException e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Artwork duplicate";
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(Exception e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Server Error";
     }
 
 }
