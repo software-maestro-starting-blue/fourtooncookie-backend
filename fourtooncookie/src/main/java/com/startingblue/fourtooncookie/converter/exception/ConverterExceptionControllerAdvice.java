c12ec749a259c17edf4b308b34f8e992e92e55fa
minseo Jo <liging12@naver.com>
2024-08-04 14:47:12 +0900

    refactor: 예외 메시지 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/exception/ConverterExceptionControllerAdvice.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/converter/exception/ConverterExceptionControllerAdvice.java
@@ -1,28 +1,35 @@
 package com.startingblue.fourtooncookie.converter.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice("com.startingblue.fourtooncookie.converter")
+@Slf4j
 public class ConverterExceptionControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleIllegalArgumentException(IllegalArgumentException e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Bad Request";
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleRuntimeExceptionException(RuntimeException e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Bad Request";
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(Exception e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Server Error";
     }
 }
