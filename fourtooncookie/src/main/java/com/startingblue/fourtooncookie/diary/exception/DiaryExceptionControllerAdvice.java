542b39d32b13f57d91c99df217fbec736585aea0
minseo Jo <liging12@naver.com>
2024-08-04 14:42:49 +0900

    refactor: 예외 메시지 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/exception/DiaryExceptionControllerAdvice.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/exception/DiaryExceptionControllerAdvice.java
@@ -2,42 +2,49 @@ package com.startingblue.fourtooncookie.diary.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice("com.startingblue.fourtooncookie.diary")
+@Slf4j
 public class DiaryExceptionControllerAdvice {
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleConstraintViolationException(ConstraintViolationException e) {
-        return "Diary constraint violation " + e.getMessage();
+        log.error(e.getMessage());
+        return "Diary constraint violation";
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleEntityNotFoundException(EntityNotFoundException e) {
-        return "Diary not found: " + e.getMessage();
+        log.error(e.getMessage());
+        return "Diary not found";
     }
 
     @ExceptionHandler(DiaryNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleDiaryNotFoundException(DiaryNotFoundException e) {
-        return "Diary not found: " + e.getMessage();
+        log.error(e.getMessage());
+        return "Diary not found";
     }
 
     @ExceptionHandler(DiaryDuplicateException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public String handleDiaryDuplicateException(DiaryDuplicateException e) {
-        return "Diary duplicate: " + e.getMessage();
+        log.error(e.getMessage());
+        return "Diary duplicate";
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(Exception e) {
-        return e.getMessage();
+        log.error(e.getMessage());
+        return "Server Error";
     }
 
 }
