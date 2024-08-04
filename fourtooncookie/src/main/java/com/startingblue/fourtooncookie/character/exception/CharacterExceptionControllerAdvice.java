79502d839d15bc1cef05bcb0cdb7af6c97f3e530
minseo Jo <liging12@naver.com>
2024-08-04 14:53:49 +0900

    refactor: 에러 메시지 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/exception/CharacterExceptionControllerAdvice.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/exception/CharacterExceptionControllerAdvice.java
@@ -2,48 +2,55 @@ package com.startingblue.fourtooncookie.character.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice("com.startingblue.fourtooncookie.character")
+@Slf4j
 public class CharacterExceptionControllerAdvice {
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
-        return ResponseEntity.badRequest().body("Validation error: " + ex.getMessage());
+    public String handleConstraintViolationException(ConstraintViolationException e) {
+        log.error(e.getMessage(), e);
+        return "Character Constraint Violation";
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleEntityNotFoundExceptionException(EntityNotFoundException e) {
-        return e.getMessage();
+        log.error(e.getMessage(), e);
+        return "Character Not Found";
     }
 
     @ExceptionHandler(CharacterNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleCharacterNoSuchElementException(CharacterNotFoundException e) {
-        return e.getMessage();
+        log.error(e.getMessage(), e);
+        return "Character Not Found";
     }
 
     @ExceptionHandler(CharacterVisionTypeNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNotFoundCharacterVisionTypeException(CharacterVisionTypeNotFoundException e) {
-        return e.getMessage();
+        log.error(e.getMessage(), e);
+        return "Character Vision Type not found";
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleIllegalArgumentException(IllegalArgumentException e) {
-        return e.getMessage();
+        log.error(e.getMessage(), e);
+        return "Bad Request";
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(Exception e) {
-        return e.getMessage();
+        log.error(e.getMessage(), e);
+        return "Character Server Error";
     }
 }
