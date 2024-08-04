de5190c8bd8cef5670a654172aba0cec8f78d857
minseo Jo <liging12@naver.com>
2024-08-04 23:42:51 +0900

    feat: GlobalExceptionControllerAdvice

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/exception/GlobalExceptionControllerAdvice.java
@@ -0,0 +1,45 @@
+package com.startingblue.fourtooncookie.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("Validation error: ", e);
+        BindingResult bindingResult = e.getBindingResult();
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return errors;
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleRuntimeException(RuntimeException e) {
+        log.error("Runtime error: ", e);
+        return "Server Error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception e) {
+        log.error("Unexpected error: ", e);
+        return "Server Error";
+    }
+
+}
