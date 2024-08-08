578db607c1cf0e2e585f29687fc9669c7907a5b5
minseo Jo <liging12@naver.com>
2024-08-08 17:31:07 +0900

    feat: HealthController (#49)
    * feat: HealthController
    
    * refactor: health 상태코드 200 반환

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/HealthController.java
@@ -0,0 +1,16 @@
+package com.startingblue.fourtooncookie.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public HttpStatus health() {
+        return HttpStatus.OK;
+    }
+}
