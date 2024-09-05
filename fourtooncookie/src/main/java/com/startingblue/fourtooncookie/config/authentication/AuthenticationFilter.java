db0ef846629324a15b94e7a470135e906c35dbe0
minseo Jo <liging12@naver.com>
2024-09-05 13:40:58 +0900

    [feat] MemberNotFoundException, AuthenticationException 에러 핸들링 추가 (#55)
    * feat: MemberNotFoundException 예외 핸들링 기능 추가
    
    * feat: jwtExtractor 에서 AuthenticationException 예외를 뱉으면 401 예외 반환
    
    필터에서 jwtExtractor를 사용하므로 스프링MVC에서 동작하는 @RestControllerAdvice가 아닌 필터 자체(서블릿)에서 catch 해서 처리

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/authentication/AuthenticationFilter.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/authentication/AuthenticationFilter.java
@@ -25,28 +25,30 @@ public class AuthenticationFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-                                                                        throws IOException, ServletException {
+            throws IOException, ServletException {
         String requestURI = request.getRequestURI();
-        if (requestURI.startsWith("/h2-console")) {
-            chain.doFilter(request, response);
-            return;
-        }
+        try {
+            if (requestURI.startsWith("/h2-console") || requestURI.startsWith("/health")) {
+                chain.doFilter(request, response);
+                return;
+            }
 
-        if (requestURI.startsWith("/health")) {
-            chain.doFilter(request, response);
-            return;
-        }
+            String token = jwtExtractor.resolveToken(request);
+            Claims claims = jwtExtractor.parseToken(token);
+            UUID memberId = UUID.fromString(claims.getSubject());
+            log.info("login attempt memberId: {}", memberId);
 
-        String token = jwtExtractor.resolveToken(request);
-        Claims claims = jwtExtractor.parseToken(token);
-        UUID memberId = UUID.fromString(claims.getSubject());
-        log.info("login attempt memberId: {}", memberId);
-        if (memberService.verifyMemberExists(memberId)) {
-            log.info("login success memberId: {}", memberId);
-            request.setAttribute("memberId", memberId);
-            chain.doFilter(request, response);
-        } else {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, String.format("Member with id %s not found", memberId));
+            if (memberService.verifyMemberExists(memberId)) {
+                log.info("login success memberId: {}", memberId);
+                request.setAttribute("memberId", memberId);
+                chain.doFilter(request, response);
+            } else {
+                log.error("Member with id {} not found", memberId);
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, String.format("Member with id %s not found", memberId));
+            }
+        } catch (AuthenticationException e) {
+            log.error("Authentication failed: {}", e.getMessage());
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication failed: " + e.getMessage());
         }
     }
 }
