5b000fa0e988f61f154f0245de03479b9ccf2f00
minseo Jo <liging12@naver.com>
2024-09-07 18:16:35 +0900

    feat: LambdaConfig 추가

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/lambda/LambdaConfig.java
@@ -0,0 +1,19 @@
+package com.startingblue.fourtooncookie.aws.lambda;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+
+@Configuration
+public class LambdaConfig {
+
+    @Bean
+    public LambdaClient lambdaClient() {
+        return LambdaClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
+}
+
+
