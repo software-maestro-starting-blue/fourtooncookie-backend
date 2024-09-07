3d61f722562991261dfef31159173ffdd9cd4cd3
minseo Jo <liging12@naver.com>
2024-09-07 11:34:27 +0900

    feat: 다이어리 paintingImageUrls 기본 이미지 제거

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiaryPaintingImagesUpdateRequest.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/dto/request/DiaryPaintingImagesUpdateRequest.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public record DiaryPaintingImagesUpdateRequest(
         @NotEmptyList(message = "일기 그림 URL 목록은 최소 1개를 포함해야 합니다.")
-        @Size(min = 1, max = 4, message = "일기 그림 URL 목록은 1개에서 4개 사이여야 합니다.")
+        @Size(max = 4, message = "일기 그림은 최대 4개 입니다.")
         List<URL> paintingImageUrls){
 }
