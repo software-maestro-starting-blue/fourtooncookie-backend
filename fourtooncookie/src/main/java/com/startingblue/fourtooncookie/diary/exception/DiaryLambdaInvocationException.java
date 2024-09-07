74c4f02bdfc4552145044aba4e3e0c3040573403
minseo Jo <liging12@naver.com>
2024-09-07 18:38:36 +0900

    feat: DiaryLambdaInvocationException
    일기 람다 예외 기능 추가

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/exception/DiaryLambdaInvocationException.java
@@ -0,0 +1,8 @@
+package com.startingblue.fourtooncookie.diary.exception;
+
+public class DiaryLambdaInvocationException extends RuntimeException {
+
+    public DiaryLambdaInvocationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
