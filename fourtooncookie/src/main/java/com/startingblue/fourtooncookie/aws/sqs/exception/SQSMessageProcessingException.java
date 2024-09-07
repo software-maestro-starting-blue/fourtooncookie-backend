f7c7d69a11c98888b6b2d3b65535011c2774e644
minseo Jo <liging12@naver.com>
2024-09-07 21:46:32 +0900

    [feat] llm, SQS, vision 패키지 제거 (#81)
    feat: llm, SQS, vision 패키지 제거

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/sqs/exception/SQSMessageProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.startingblue.fourtooncookie.aws.sqs.exception;
-
-public class SQSMessageProcessingException extends RuntimeException {
-    public SQSMessageProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
