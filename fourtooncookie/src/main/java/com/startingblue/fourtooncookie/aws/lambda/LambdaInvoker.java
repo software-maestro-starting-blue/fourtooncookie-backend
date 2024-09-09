c1c91deecffbfe64c57fc0cc2ac91d68eb8c096c
minseo Jo <liging12@naver.com>
2024-09-09 17:44:56 +0900

    [hotfix] 람다호출을 자바 코드로 비동기 처리한 것을, 람다 invocationType 변경을 통해 비동기로 변경 (#97)
    hotfix: 람다호출을 자바 코드로 비동기 처리한 것을, 람다 invocationType 변경을 통해 비동기로 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/lambda/LambdaInvoker.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/lambda/LambdaInvoker.java
@@ -3,6 +3,7 @@ package com.startingblue.fourtooncookie.aws.lambda;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.InvocationType;
 import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 import software.amazon.awssdk.core.SdkBytes;
 
@@ -16,6 +17,7 @@ public class LambdaInvoker {
         InvokeRequest invokeRequest = InvokeRequest.builder()
                 .functionName(functionName)
                 .payload(SdkBytes.fromUtf8String(payload))
+                .invocationType(InvocationType.EVENT)
                 .build();
 
         lambdaClient.invoke(invokeRequest);
