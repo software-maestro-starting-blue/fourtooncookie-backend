f306cf34d1b50e4cd6624431364418d5a9198e99
minseo Jo <liging12@naver.com>
2024-09-09 23:41:36 +0900

    fix: 람다 요청 및 읽기 시간 변경 (#113)
    fix

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/LambdaConfig.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/LambdaConfig.java
@@ -3,10 +3,12 @@ package com.startingblue.fourtooncookie.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
-import software.amazon.awssdk.core.retry.RetryMode;
 import software.amazon.awssdk.core.retry.RetryPolicy;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class LambdaConfig {
@@ -18,7 +20,15 @@ public class LambdaConfig {
         return LambdaClient.builder()
                 .overrideConfiguration(
                         ClientOverrideConfiguration.builder()
-                                .retryPolicy(RetryPolicy.none())
+                                .retryPolicy(RetryPolicy.none()) // Retry 설정 (필요에 따라 변경 가능)
+                                .apiCallTimeout(Duration.ofMinutes(2)) // 전체 API 호출 타임아웃 설정
+                                .apiCallAttemptTimeout(Duration.ofMinutes(2)) // 개별 호출 시도에 대한 타임아웃 설정
+                                .build()
+                )
+                .httpClient(
+                        ApacheHttpClient.builder()
+                                .socketTimeout(Duration.ofMinutes(1)) // 읽기 타임아웃 설정
+                                .connectionTimeout(Duration.ofSeconds(120)) // 연결 타임아웃 설정
                                 .build()
                 )
                 .region(region)
