36aff9cb5552487606fb24f13fa397b779d17103
minseo Jo <liging12@naver.com>
2024-09-09 17:10:03 +0900

    [bug] 일기 이미지 생성 람다호출 json 생성, ObjectMapper로 변경 (#95)
    * htotfix: 이미지 생성 람다호출 json 생성, ObjectMapper로 변경
    
    * refactor: DiaryImageGenerationCharacterResponse -> DiaryImageGenerationCharacterPayload
    
    ---------
    
    Co-authored-by: JuneBridge <junebridge03@g.skku.edu>

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/lambda/diaryImageGenerationPayload/DiaryImageGenerationCharacterPayload.java
@@ -0,0 +1,4 @@
+package com.startingblue.fourtooncookie.aws.lambda.diaryImageGenerationPayload;
+
+public record DiaryImageGenerationCharacterPayload(Long id, String name, String visionType, String basePrompt) {
+}
