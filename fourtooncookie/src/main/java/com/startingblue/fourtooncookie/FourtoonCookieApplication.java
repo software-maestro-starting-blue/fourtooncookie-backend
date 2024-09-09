18b4330330e597e1e3feb6f53a2948894f6b37b1
minseo Jo <liging12@naver.com>
2024-09-09 21:30:26 +0900

    hotfix: @EnableAsync

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/FourtoonCookieApplication.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/FourtoonCookieApplication.java
@@ -3,10 +3,12 @@ package com.startingblue.fourtooncookie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class FourtoonCookieApplication {
 
