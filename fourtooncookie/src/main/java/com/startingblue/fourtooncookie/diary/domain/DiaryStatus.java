c81b4f772abfd5d190dd80c21a35832d5e8b900d
minseo Jo <liging12@naver.com>
2024-09-12 16:47:58 +0900

    refactor: DiaryStatus 주석 제거

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/domain/DiaryStatus.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/domain/DiaryStatus.java
@@ -1,16 +1,7 @@
 package com.startingblue.fourtooncookie.diary.domain;
 
-import lombok.Getter;
-
-@Getter
 public enum DiaryStatus {
-    IN_PROGRESS("생성 중"),
-    COMPLETED("완료"),
-    FAILED("생성 실패");
-
-    private final String description;
-
-    DiaryStatus(String description) {
-        this.description = description;
-    }
+    IN_PROGRESS,
+    COMPLETED,
+    FAILED
 }
