4600c30895f0c40fd2b047f3da4902974765bce3
minseo Jo <liging12@naver.com>
2024-09-13 16:40:14 +0900

    feat: DiaryLambdaCallEvent 제거

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/event/domain/DiaryLambdaCallEvent.java
+++
@@ -1,7 +0,0 @@
-package com.startingblue.fourtooncookie.event.domain;
-
-import com.startingblue.fourtooncookie.character.domain.Character;
-import com.startingblue.fourtooncookie.diary.domain.Diary;
-
-public record DiaryLambdaCallEvent(Diary diary, Character character) {
-}
