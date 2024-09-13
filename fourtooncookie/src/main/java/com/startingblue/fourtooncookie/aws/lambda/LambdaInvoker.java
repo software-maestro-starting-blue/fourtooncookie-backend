4b8cb379d2c4f6e12ffe3b6fbea30697c10baf79
minseo Jo <liging12@naver.com>
2024-09-13 16:40:28 +0900

    feat: LambdaInvoker

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/lambda/LambdaInvoker.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/lambda/LambdaInvoker.java
@@ -1,72 +1,44 @@
 package com.startingblue.fourtooncookie.aws.lambda;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.startingblue.fourtooncookie.aws.lambda.diaryImageGenerationPayload.DiaryImageGenerationCharacterPayload;
-import com.startingblue.fourtooncookie.aws.lambda.diaryImageGenerationPayload.DiaryImageGenerationLambdaPayload;
-import com.startingblue.fourtooncookie.character.domain.Character;
-import com.startingblue.fourtooncookie.diary.domain.Diary;
-import com.startingblue.fourtooncookie.diary.exception.DiaryLambdaInvocationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Async;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.InvocationType;
 import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 
-@Service
 @RequiredArgsConstructor
 @Slf4j
-public class LambdaInvoker {
-
-    private static final String IMAGE_GENERATE_LAMBDA_FUNCTION_NAME = "fourtooncookie-diaryimage-ai-apply-lambda";
+public abstract class LambdaInvoker {
 
     private final LambdaClient lambdaClient;
-    private final ObjectMapper objectMapper;
 
-    public void invokeImageGenerateLambdaAsync(Diary diary, Character character) {
-        String payload = buildLambdaPayload(diary, character);
+    @Async
+    public void invokeLambdaAsync(Object payload) {
         try {
-            invokeLambda(IMAGE_GENERATE_LAMBDA_FUNCTION_NAME, payload);
-            log.info("Lambda 호출 성공: 일기 ID - {}", diary.getId());
-        } catch (RuntimeException e) {
-            throw e;
+            String serializedPayload = serializePayload(payload);
+            invokeLambda(serializedPayload);
         } catch (Exception e) {
-            log.error("Lambda invoke error", e);
+            log.error("Lambda 호출 중 오류 발생: {}", e.getMessage());
+            throw new RuntimeException("Lambda 호출 실패", e);
         }
     }
 
-    public void invokeLambda(String functionName, String payload) {
+    protected abstract String serializePayload(Object payload);
+
+    private void invokeLambda(String payload) {
         InvokeRequest invokeRequest = InvokeRequest.builder()
-                .functionName(functionName)
+                .functionName(getFunctionName())
                 .payload(SdkBytes.fromUtf8String(payload))
                 .invocationType(InvocationType.REQUEST_RESPONSE)
                 .build();
 
         var response = lambdaClient.invoke(invokeRequest);
         if (response.payload().asUtf8String().equals("false")) {
-            throw new RuntimeException("Lambda invocation failed");
+            throw new RuntimeException("Lambda 호출 실패");
         }
     }
 
-    private String buildLambdaPayload(Diary diary, Character character) {
-        DiaryImageGenerationCharacterPayload characterResponse = new DiaryImageGenerationCharacterPayload(
-                character.getId(),
-                character.getName(),
-                character.getCharacterVisionType().name(),
-                character.getBasePrompt()
-        );
-
-        DiaryImageGenerationLambdaPayload payload = new DiaryImageGenerationLambdaPayload(
-                diary.getId(),
-                diary.getContent(),
-                characterResponse
-        );
-        try {
-            return objectMapper.writeValueAsString(payload);
-        } catch (JsonProcessingException e) {
-            throw new DiaryLambdaInvocationException("Lambda payload 직렬화 중 오류가 발생했습니다.", e);
-        }
-    }
+    protected abstract String getFunctionName();
 }
