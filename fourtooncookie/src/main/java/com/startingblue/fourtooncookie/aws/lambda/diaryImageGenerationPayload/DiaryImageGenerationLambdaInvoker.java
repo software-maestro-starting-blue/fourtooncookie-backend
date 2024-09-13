aa25eca3693bd763de0077e5271b1f429d1d7261
minseo Jo <liging12@naver.com>
2024-09-13 19:38:28 +0900

    fix: @Async 내부클래스에서 안되는 버그 수정

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/lambda/diaryImageGenerationPayload/DiaryImageGenerationLambdaInvoker.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/lambda/diaryImageGenerationPayload/DiaryImageGenerationLambdaInvoker.java
@@ -5,34 +5,50 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.startingblue.fourtooncookie.aws.lambda.LambdaInvoker;
 import com.startingblue.fourtooncookie.character.domain.Character;
 import com.startingblue.fourtooncookie.diary.domain.Diary;
+import com.startingblue.fourtooncookie.diary.domain.DiaryRepository;
+import com.startingblue.fourtooncookie.diary.domain.DiaryStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.InvocationType;
 
 @Service
+@Slf4j
 public class DiaryImageGenerationLambdaInvoker extends LambdaInvoker {
 
     private static final String FUNCTION_NAME = "fourtooncookie-diaryimage-ai-apply-lambda";
 
     private final ObjectMapper objectMapper;
 
-    public DiaryImageGenerationLambdaInvoker(LambdaClient lambdaClient, ObjectMapper objectMapper1) {
+    private final DiaryRepository diaryRepository;
+
+    public DiaryImageGenerationLambdaInvoker(LambdaClient lambdaClient, ObjectMapper objectMapper, DiaryRepository diaryRepository) {
         super(lambdaClient, FUNCTION_NAME, InvocationType.REQUEST_RESPONSE);
-        this.objectMapper = objectMapper1;
+        this.objectMapper = objectMapper;
+        this.diaryRepository = diaryRepository;
     }
 
-    public void invokeDiaryImageGenerationLambda(Diary diary, Character character) throws RuntimeException {
-        DiaryImageGenerationLambdaPayload diaryImageGenerationLambdaPayload = buildPayload(diary, character);
-        String serializePayload = serializePayload(diaryImageGenerationLambdaPayload);
-        invokeLambda(serializePayload);
-    }
-
-    private String serializePayload(DiaryImageGenerationLambdaPayload payload) {
+    @Async
+    @Transactional
+    public void invokeDiaryImageGenerationLambda(Diary diary, Character character) {
+        DiaryStatus status = DiaryStatus.IN_PROGRESS;
         try {
-            return objectMapper.writeValueAsString(payload);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("페이로드 직렬화 중 오류 발생", e);
+            DiaryImageGenerationLambdaPayload diaryImageGenerationLambdaPayload = buildPayload(diary, character);
+            String serializePayload = serializePayload(diaryImageGenerationLambdaPayload);
+            invokeLambda(serializePayload);
+            status = DiaryStatus.COMPLETED;
+        } catch (Exception e) {
+            log.error("Lambda 호출 중 오류 발생: {}", e.getMessage());
+            status = DiaryStatus.FAILED;
+        } finally {
+            handleLambdaResult(diary, status);
         }
+    }
+
+    private String serializePayload(DiaryImageGenerationLambdaPayload payload) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(payload);
     }
 
     private DiaryImageGenerationLambdaPayload buildPayload(Diary diary, Character character) {
@@ -46,5 +62,10 @@ public class DiaryImageGenerationLambdaInvoker extends LambdaInvoker {
                         character.getBasePrompt()
                 )
         );
+    }
+
+    private void handleLambdaResult(Diary diary, DiaryStatus status) {
+        diary.updateDiaryStatus(status);
+        diaryRepository.save(diary);
     }
 }
