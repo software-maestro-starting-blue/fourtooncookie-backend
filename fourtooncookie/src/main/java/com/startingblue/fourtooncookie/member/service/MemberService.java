0f0d69333fc330325042dd1d2626b7a7e4d17223
minseo Jo <liging12@naver.com>
2024-09-21 14:31:20 +0900

    fix: 잘못된 커밋 복구

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/service/MemberService.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/member/service/MemberService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -40,6 +41,16 @@ public class MemberService {
 
     public void hardDeleteById(UUID memberId) {
         memberRepository.deleteById(memberId);
+    }
+
+    public void softDeleteById(UUID memberId) {
+        softDeleteById(memberId, LocalDateTime.now());
+    }
+
+    public void softDeleteById(UUID memberId, LocalDateTime current) {
+        Member foundMember = readById(memberId);
+        foundMember.softDelete(current);
+        memberRepository.save(foundMember);
     }
 
     public boolean verifyMemberExists(UUID memberId) {
