940a36fa01ed8f5bc9cf8387a4c4edf13d12aaec
minseo Jo <liging12@naver.com>
2024-10-01 04:29:22 +0900

    feat: DiaryCloudFrontService

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/diary/service/DiaryCloudFrontService.java
@@ -0,0 +1,29 @@
+package com.startingblue.fourtooncookie.diary.service;
+
+import com.startingblue.fourtooncookie.aws.cloudfront.CloudFrontService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DiaryCloudFrontService {
+
+    private final CloudFrontService cloudFrontService;
+
+    public Map<String, String> generateSignedCookies(UUID memberId) {
+        try {
+            String path = memberId.toString();
+            return cloudFrontService.generateCloudFrontSignedCookies(path);
+        } catch (Exception e) {
+            log.error("Failed to generate signed cookies for memberId: {}", memberId, e);
+            return null;
+        }
+    }
+
+}
