2085ff828511917c3d0b6fda5c69ec92ff587a30
minseo Jo <liging12@naver.com>
2024-10-04 00:30:03 +0900

    feat: SQSConfig

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/sqs/SQSConfig.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/aws/sqs/SQSConfig.java
@@ -1,11 +1,14 @@
 package com.startingblue.fourtooncookie.aws.sqs;
 
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import io.awspring.cloud.sqs.listener.acknowledgement.AcknowledgementOrdering;
+import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-import software.amazon.awssdk.services.sqs.SqsClient;
+
+import java.time.Duration;
 
 @Configuration
 public class SQSConfig {
@@ -13,9 +16,24 @@ public class SQSConfig {
     private static final Region region = Region.AP_NORTHEAST_2;
 
     @Bean
-    public SqsAsyncClient sqsClient() {
+    public SqsAsyncClient sqsAsyncClient() {
         return SqsAsyncClient.builder()
                 .region(region)
                 .build();
     }
+
+    @Bean
+    public SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory() {
+        return SqsMessageListenerContainerFactory
+                .builder()
+                .configure(options -> options
+                        .acknowledgementMode(AcknowledgementMode.ON_SUCCESS)
+                        .acknowledgementInterval(Duration.ofSeconds(5))
+                        .acknowledgementThreshold(4)
+                        .acknowledgementOrdering(AcknowledgementOrdering.ORDERED)
+                )
+                .sqsAsyncClient(sqsAsyncClient())
+                .build();
+    }
+
 }
