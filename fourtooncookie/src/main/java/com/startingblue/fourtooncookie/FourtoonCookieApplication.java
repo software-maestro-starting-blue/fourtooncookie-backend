99d90ca1953f1d04baf87c1272244ae50c68499c
minseo Jo <liging12@naver.com>
2024-10-04 00:30:26 +0900

    chore: 필요없는 코드 제거

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/FourtoonCookieApplication.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/FourtoonCookieApplication.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @EnableAsync
-@EnableSqs
 @SpringBootApplication
 public class FourtoonCookieApplication {
 
