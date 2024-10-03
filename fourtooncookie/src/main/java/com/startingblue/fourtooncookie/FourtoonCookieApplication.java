fc6316e435728f4c22353dd3e916b49c5d1e9d79
minseo Jo <liging12@naver.com>
2024-10-04 00:30:42 +0900

    chore: 필요없는 코드 제거

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/FourtoonCookieApplication.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/FourtoonCookieApplication.java
@@ -2,7 +2,6 @@ package com.startingblue.fourtooncookie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
