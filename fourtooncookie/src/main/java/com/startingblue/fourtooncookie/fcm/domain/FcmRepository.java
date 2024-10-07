e2c7414b5b4934e3061c7090f6318331185e9f0a
minseo Jo <liging12@naver.com>
2024-10-07 18:26:11 +0900

    feat: FcmRepository

---
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/fcm/domain/FcmRepository.java
@@ -0,0 +1,12 @@
+package com.startingblue.fourtooncookie.fcm.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface FcmRepository extends JpaRepository<FcmToken, Long> {
+
+    @Query("SELECT f FROM FcmToken f JOIN f.diaries d WHERE d.id = :diaryId")
+    Optional<FcmToken> findByDiaryId(Long diaryId);
+}
