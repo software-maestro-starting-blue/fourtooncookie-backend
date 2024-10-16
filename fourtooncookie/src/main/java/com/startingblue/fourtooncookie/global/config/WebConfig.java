c4db80aa8c9d3f22a4a9bfbca7ca1633ab6e467f
minseo Jo <liging12@naver.com>
2024-10-17 02:18:43 +0900

    refactor: LocaleChangeInterceptor 필요없는 lang 파라미터 제거
    Accept-Language 가 있고 Accept-Language 벨류 값이 지원하는 언어(ko, en)인 경우 로케일 설정, 그 외의 경우 영어 기본 값

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/global/config/WebConfig.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.startingblue.fourtooncookie.global.config;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
@@ -12,16 +13,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final List<String> SUPPORTED_LANGUAGES = Arrays.asList("ko", "en");
+
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(Locale.ENGLISH); // 기본 로케일 설정
-        return localeResolver;
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH); // 기본 로케일 설정
+        return resolver;
     }
 
     @Bean
@@ -29,26 +34,17 @@ public class WebConfig implements WebMvcConfigurer {
         return new LocaleChangeInterceptor() {
             @Override
             public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException {
-                String langParam = request.getParameter("lang");
-                if (!StringUtils.hasText(langParam)) {
-                    // Accept-Language 헤더 우선 처리
-                    langParam = request.getHeader("Accept-Language");
-                    if (langParam == null) {
-                        langParam = "en"; // 기본 값 영어로
-                    } else {
-                        // Accept-Language에서 첫 번째 언어를 선택 (Accept-Language: ko, en;q=0.8, ja;q=0.6) 헤더 정보가 형태임
-                        langParam = langParam.split(",")[0].split("-")[0];
+                String language = request.getHeader("Accept-Language");
+                Locale locale = Locale.ENGLISH;
+
+                if (StringUtils.hasText(language)) {
+                    language = language.split(",")[0].split("-")[0];
+                    if (SUPPORTED_LANGUAGES.contains(language)) {
+                        locale = new Locale(language);
                     }
                 }
 
-                LocaleResolver localeResolver = localeResolver();
-                if ("ko".equals(langParam)) {
-                    localeResolver.setLocale(request, response, Locale.KOREA);
-                } else if ("en".equals(langParam)) {
-                    localeResolver.setLocale(request, response, Locale.ENGLISH);
-                } else {
-                    localeResolver.setLocale(request, response, Locale.ENGLISH); // 기본값 설정
-                }
+                localeResolver().setLocale(request, response, locale);
 
                 return super.preHandle(request, response, handler);
             }
