bb518940c213562339086b7b02e78f994680f159
minseo Jo <liging12@naver.com>
2024-10-16 21:15:54 +0900

    feat: XmlMessageSource 빈 등록

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/global/config/MessageConfig.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/global/config/MessageConfig.java
@@ -3,21 +3,12 @@ package com.startingblue.fourtooncookie.global.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-
-import java.util.Locale;
 
 @Configuration
 public class MessageConfig {
 
     @Bean
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames("classpath:messages/messages");
-        messageSource.setFallbackToSystemLocale(false);
-        messageSource.setAlwaysUseMessageFormat(true);
-        messageSource.setDefaultLocale(Locale.ENGLISH);
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
+        return new XmlMessageSource();
     }
 }
