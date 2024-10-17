2ba53f3001a4080cecb0fffb5da1ca54ef97dd0b
minseo Jo <liging12@naver.com>
2024-10-17 21:40:46 +0900

    refactor: LocaleConfig

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/global/config/LocaleConfig.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/global/config/LocaleConfig.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.http.HttpHeaders;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,38 +14,41 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer {
 
-    private static final List<String> SUPPORTED_LANGUAGES = Arrays.asList("ko", "en");
+    private static final Locale DEFAULT_LANGUAGE = Locale.ENGLISH;
+    private static final Map<String, String> SUPPORTED_LANGUAGES = new HashMap<>();
 
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver resolver = new SessionLocaleResolver();
-        resolver.setDefaultLocale(Locale.ENGLISH); // 기본 로케일 설정
-        return resolver;
+    static {
+        SUPPORTED_LANGUAGES.put("ko", "ko");
+        SUPPORTED_LANGUAGES.put("ko-KR", "ko");
     }
 
     @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(LocaleResolver localeResolver) {
         return new LocaleChangeInterceptor() {
             @Override
             public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException {
-                String language = request.getHeader("Accept-Language");
-                Locale locale = Locale.ENGLISH;
+                String language = request.getHeader(HttpHeaders.ACCEPT_LANGUAGE);
+                Locale locale = DEFAULT_LANGUAGE; // 기본 로케일 설정
 
                 if (StringUtils.hasText(language)) {
-                    language = language.split(",")[0].split("-")[0];
-                    if (SUPPORTED_LANGUAGES.contains(language)) {
-                        locale = new Locale(language);
+                    language = language.split(",")[0];
+                    System.out.println(language);
+                    if (SUPPORTED_LANGUAGES.containsKey(language)) {
+                        locale = Locale.forLanguageTag(SUPPORTED_LANGUAGES.get(language));
                     }
                 }
 
-                localeResolver().setLocale(request, response, locale);
+                localeResolver.setLocale(request, response, locale);
 
                 return super.preHandle(request, response, handler);
             }
@@ -52,6 +57,6 @@ public class LocaleConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
+        registry.addInterceptor(localeChangeInterceptor(localeResolver()));
     }
 }
