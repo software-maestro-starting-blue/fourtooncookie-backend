b2b45868f1a3bbab2513ea0ee9783a938de7c239
minseo Jo <liging12@naver.com>
2024-10-18 13:28:21 +0900

    refactor: CharacterException

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/exception/CharacterNotFoundException.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/exception/CharacterNotFoundException.java
@@ -1,7 +1,11 @@
 package com.startingblue.fourtooncookie.character.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.NoSuchElementException;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public final class CharacterNotFoundException extends NoSuchElementException {
 
     public CharacterNotFoundException(String message) {
--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/exception/CharacterVisionTypeNotFoundException.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/character/exception/CharacterVisionTypeNotFoundException.java
@@ -1,7 +1,11 @@
 package com.startingblue.fourtooncookie.character.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.NoSuchElementException;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public final class CharacterVisionTypeNotFoundException extends NoSuchElementException {
     public CharacterVisionTypeNotFoundException(String message) {
         super("Invalid CharacterVisionType value: " + message);
