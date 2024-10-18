b6ee4af94b99b4f507aad983927cf0fe4134ee97
minseo Jo <liging12@naver.com>
2024-10-18 11:29:50 +0900

    refactor: LocaleResvoler 구현체 변경

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/LocaleConfig.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/config/LocaleConfig.java
@@ -1,31 +1,29 @@
 package com.startingblue.fourtooncookie.config;
 
-import com.startingblue.fourtooncookie.web.interceptor.CustomLocaleChangeInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.List;
+import java.util.Locale;
 
 
 @Configuration
 @RequiredArgsConstructor
 public class LocaleConfig implements WebMvcConfigurer {
 
+    private static final List<Locale> SUPPORTED_LOCALES = List.of(Locale.KOREAN, Locale.KOREA);
+
     @Bean
     public LocaleResolver localeResolver() {
-        return new SessionLocaleResolver();
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        localeResolver.setSupportedLocales(SUPPORTED_LOCALES);
+        return localeResolver;
     }
 
-    @Bean
-    public CustomLocaleChangeInterceptor localeChangeInterceptor() {
-        return new CustomLocaleChangeInterceptor(localeResolver());
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
-    }
 }
