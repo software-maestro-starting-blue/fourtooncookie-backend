bc088486d7760f00970ff4bf73ec5bb531cf1137
minseo Jo <liging12@naver.com>
2024-10-18 13:26:50 +0900

    refactor: ArtworkException

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/exception/ArtworkDuplicateException.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/exception/ArtworkDuplicateException.java
@@ -1,5 +1,9 @@
 package com.startingblue.fourtooncookie.artwork.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ArtworkDuplicateException extends RuntimeException{
     public ArtworkDuplicateException(String message) {
         super(message);
