809766e2c4586ff86b9759b756c7e11ca76a5b03
minseo Jo <liging12@naver.com>
2024-10-18 13:32:57 +0900

    refactor: ConversionException

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/global/converter/exception/ConversionException.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/global/converter/exception/ConversionException.java
@@ -1,5 +1,9 @@
 package com.startingblue.fourtooncookie.global.converter.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class ConversionException extends RuntimeException {
     public ConversionException(String message, Throwable cause) {
         super(message, cause);
