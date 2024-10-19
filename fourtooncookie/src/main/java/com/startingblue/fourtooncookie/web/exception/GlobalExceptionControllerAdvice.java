122667a3807c409ee73d44975c636b36bbee8d80
minseo Jo <liging12@naver.com>
2024-10-19 21:30:53 +0900

    hotfix: Eception 못잡는 버그 수정

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/web/exception/GlobalExceptionControllerAdvice.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/web/exception/GlobalExceptionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.startingblue.fourtooncookie.web.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 @Slf4j
@@ -28,8 +30,4 @@ public class GlobalExceptionControllerAdvice {
         return errors;
     }
 
-    @ExceptionHandler(Exception.class)
-    public void handleException(Exception e) {
-        log.error(e.getMessage(), e);
-    }
 }
