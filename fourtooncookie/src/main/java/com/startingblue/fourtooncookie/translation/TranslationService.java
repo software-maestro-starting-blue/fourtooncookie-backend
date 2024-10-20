7f6021d3349f0b595c4a1d547241dfd53119f139
JuneBridge <junebridge03@g.skku.edu>
2024-10-21 01:12:01 +0900

    refactor: TranslationService 매서드 정리 + camel -> snake 변환 과정 삭제
    굳이 data에서까지 snake_case를 고집할 필요성은 없다고 생각해서 로직을 뺐습니다.
    또한 오히려 camel -> snake하는 과정이 로직을 더 번잡하게 만들기도 하여서 제거했습니다.

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/translation/TranslationService.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/translation/TranslationService.java
@@ -20,46 +20,25 @@ public class TranslationService {
     private final TranslationRepository translationRepository;
 
     public <T> T getTranslatedObject(T object, Locale locale) {
-        String className = getClassName(object);
-        Long classId = getClassId(object);
-
         Arrays.stream(object.getClass().getDeclaredFields())
             .filter(field -> field.isAnnotationPresent(TranslatableField.class))
             .forEach(field -> {
                 try {
                     field.setAccessible(true);
 
-                    String fieldName = camelToSnake(field.getName());
+                    String fieldName = field.getName();
                     Object fieldValue = field.get(object);
 
                     if (!(fieldValue instanceof String))
                         return;
 
-                    String translatedValue = getTranslationContent(className, fieldName, classId, locale);
+                    String translatedValue = getTranslationContent(object, fieldName, locale);
 
                     field.set(object, translatedValue);
                 } catch (Exception ignored) {}
             });
 
         return object;
-    }
-
-    private String getClassName(Object object) {
-        return camelToSnake(object.getClass().getSimpleName());
-    }
-
-    private Long getClassId(Object object) {
-        try {
-            return (Long) Arrays.stream(object.getClass().getDeclaredFields())
-                    .filter(field -> field.isAnnotationPresent(Id.class))
-                    .findFirst().get().get(object);
-        } catch (Exception e) {
-            throw new TranslationObjectClassIdNotFoundException();
-        }
-    }
-
-    private String camelToSnake(String camelCaseStr) {
-        return camelCaseStr.replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
     }
 
     public String getTranslationContent(TranslationId translationId) {
@@ -71,6 +50,12 @@ public class TranslationService {
 
     public String getTranslationContent(String className, String fieldName, Long classId, Locale locale) {
         return getTranslationContent(new TranslationId(className, fieldName, classId, locale));
+    }
+
+    public String getTranslationContent(Object object, String fieldName, Locale locale) {
+        String className = getClassName(object);
+        Long classId = getClassId(object);
+        return getTranslationContent(className, fieldName, classId, locale);
     }
 
     public void addTranslation(Object object, String fieldName, Locale locale, String content) {
@@ -93,6 +78,20 @@ public class TranslationService {
 
     private boolean isTranslationExists(TranslationId translationId) {
         return translationRepository.existsById(translationId);
+    }
+
+    private String getClassName(Object object) {
+        return object.getClass().getSimpleName();
+    }
+
+    private Long getClassId(Object object) {
+        try {
+            return (Long) Arrays.stream(object.getClass().getDeclaredFields())
+                    .filter(field -> field.isAnnotationPresent(Id.class))
+                    .findFirst().get().get(object);
+        } catch (Exception e) {
+            throw new TranslationObjectClassIdNotFoundException();
+        }
     }
 
 }
