9372709d3f481e2632656d48476afafabae7cb02
JuneBridge <junebridge03@g.skku.edu>
2024-10-20 21:39:53 +0900

    feat: Artwork 도메인에 @TranslatableField 어노테이션 적용

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/domain/Artwork.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/artwork/domain/Artwork.java
@@ -1,11 +1,13 @@
 package com.startingblue.fourtooncookie.artwork.domain;
 
+import com.startingblue.fourtooncookie.translation.annotation.TranslatableField;
 import jakarta.persistence.*;
 import jakarta.validation.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +20,7 @@ import java.util.Set;
 @Slf4j
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Artwork {
 
     @Id
@@ -27,6 +30,7 @@ public class Artwork {
 
     @NotBlank(message = "작품명은 필수 입니다.")
     @Size(min = 1, max = 255, message = "작품명의 글자수는 1에서 255자 이내여야 합니다.")
+    @TranslatableField
     private String title;
 
     @NotNull(message = "작품 썸네일 URL이 존재해야 합니다.")
@@ -37,12 +41,6 @@ public class Artwork {
         this.title = title;
         this.thumbnailUrl = thumbnailUrl;
         validate();
-    }
-
-    public Artwork(final Long id, final String title, final URL thumbnailUrl) {
-        this.id = id;
-        this.title = title;
-        this.thumbnailUrl = thumbnailUrl;
     }
 
     public void update(final String title, final URL thumbnailUrl) {
@@ -71,5 +69,10 @@ public class Artwork {
     @Override
     public int hashCode() {
         return Objects.hash(id, title, thumbnailUrl);
+    }
+
+    @Override
+    public Artwork clone() {
+        return new Artwork(id, title, thumbnailUrl);
     }
 }
