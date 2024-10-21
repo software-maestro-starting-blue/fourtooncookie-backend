d9bc8d89b208f48c0ac7170753dc9fd50d11e7f9
SproutMj <mjk54565@gmail.com>
2024-10-21 18:11:46 +0900

    feat: 알림 전송 시 토큰에 따라 언어를 변경하도록 구현

--- fourtooncookie/src/main/java/com/startingblue/fourtooncookie/notification/NotificationService.java
+++ fourtooncookie/src/main/java/com/startingblue/fourtooncookie/notification/NotificationService.java
@@ -1,18 +1,24 @@
 package com.startingblue.fourtooncookie.notification;
 
 import com.startingblue.fourtooncookie.diary.domain.Diary;
+import com.startingblue.fourtooncookie.locale.XmlMessageSource;
 import com.startingblue.fourtooncookie.notification.domain.NotificationToken;
 import com.startingblue.fourtooncookie.notification.dto.NotificationTokenAssignRequest;
 import com.startingblue.fourtooncookie.notification.dto.NotificationTokenUnassignRequest;
 import com.startingblue.fourtooncookie.notification.exeption.NotificationSendException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -26,6 +32,7 @@ public class NotificationService {
 
     private static final String EXPO_PUSH_SEND_API = "https://exp.host/--/api/v2/push/send";
     private final NotificationTokenRepository notificationTokenRepository;
+    private final XmlMessageSource xmlMessageSource;
 
     public void assignNotificationTokenToMember(final Locale locale, final UUID memberId, final NotificationTokenAssignRequest notificationTokenAssignRequest) {
         notificationTokenRepository.findByToken(notificationTokenAssignRequest.notificationToken())
@@ -40,18 +47,33 @@ public class NotificationService {
     public void sendNotificationToMember(final UUID memberId, final Diary diary) {
         final List<NotificationToken> notificationTokens = notificationTokenRepository.findByMemberId(memberId);
 
+        final Map<Locale, List<String>> pushNotification = new HashMap<>();
+        notificationTokens.forEach(token -> {
+            if (!pushNotification.containsKey(token.getLocale())) {
+                pushNotification.put(token.getLocale(), new ArrayList<>());
+            }
+            pushNotification.get(token.getLocale()).add(token.getToken());
+        });
+        
+        for (final Locale locale : pushNotification.keySet()) {
+            final String title = xmlMessageSource.resolveCode("notification.title." + diary.getStatus().toString().toLowerCase(), locale).toString();
+            final String content = xmlMessageSource.resolveCode("notification.content." + diary.getStatus().toString().toLowerCase(), locale).toString();
+
+            sendMessageByPushMessage(pushNotification.get(locale), title, content);
+        }
+    }
+
+    private void sendMessageByPushMessage(final List<String> to, final String title, final String body) {
         final Map<String, Object> pushMessage = new HashMap<>();
-        final List<String> list = notificationTokens.stream().map(NotificationToken::getToken).toList();
-        pushMessage.put("to", list);
-        pushMessage.put("title", "제목");
-        pushMessage.put("body", "내용");
+        pushMessage.put("to", to);
+        pushMessage.put("title", title);
+        pushMessage.put("body", body);
 
         final RestTemplate restTemplate = new RestTemplate();
-        final String url = EXPO_PUSH_SEND_API;
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         final HttpEntity<Map<String, Object>> entity = new HttpEntity<>(pushMessage, headers);
-        final ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+        final ResponseEntity<String> exchange = restTemplate.exchange(EXPO_PUSH_SEND_API, HttpMethod.POST, entity, String.class);
 
         if (!exchange.getStatusCode().is2xxSuccessful()) {
             throw new NotificationSendException(exchange.getStatusCode() + exchange.getBody());
